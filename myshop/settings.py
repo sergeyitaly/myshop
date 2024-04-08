3f4ec1467a97780eca230566750367d40c68f204
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-08 23:53:38 +0300

    settings

--- myshop/settings.py
+++ myshop/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = 'django-insecure-7yp%33lazy-*44btq1zje9yyqc3+_of(b=&_asvl#f)6b#(nyq
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG") != "False"
-ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh"]
 #ALLOWED_HOSTS = ['*']
 # Application definition
 
