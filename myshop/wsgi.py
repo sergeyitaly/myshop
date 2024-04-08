a78e544ebcf7100e6476ef8e2a3fb4cfd8f8e209
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-08 23:33:44 +0300

    vercel

--- myshop/wsgi.py
+++ myshop/wsgi.py
@@ -14,5 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshop.settings')
 
 application = get_wsgi_application()
-app = application # add this line.
-
+WSGI_APPLICATION = 'vercel_app.wsgi.app'
