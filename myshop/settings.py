0394674a9d520d998af556940b461f2dad8a39ba
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-08 23:42:15 +0300

    settings

--- myshop/settings.py
+++ myshop/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
    #     'ENGINE': 'django.db.backends.sqlite3',
    #     'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+
 # CORS settings
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
