6841d66cd13338c781553732b4111835035fb17b
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-10 14:35:15 +0300

    fix

--- build_files.sh
+++ build_files.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-# Create and activate a virtual environment
+# Create a virtual environment named 'myenv'
 python3 -m venv myenv
+
+# Activate the virtual environment
 source myenv/bin/activate
 
-# Upgrade pip and install requirements
+# Upgrade pip inside the virtual environment
 python -m pip install --upgrade pip
+
+# Install requirements from requirements.txt
 pip install -r requirements.txt
 
-# Set Django settings module
-export DJANGO_SETTINGS_MODULE=myshop.settings
-
-# Collect static files
-python manage.py collectstatic --noinput
+# Collect static files (use appropriate settings)
+python manage.py collectstatic --noinput --clear --verbosity 0
