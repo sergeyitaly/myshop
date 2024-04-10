ac13fbdfd8f33be977142af8ad01eb3ff52f2db3
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-10 12:54:45 +0300

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,15 @@
+#!/bin/bash
 
-# build_files.sh
+# Create a virtual environment named 'myenv'
+python3 -m venv myenv
+
+# Activate the virtual environment
+source myenv/bin/activate
+
+# Upgrade pip inside the virtual environment
+python -m pip install --upgrade pip
+
+# Install requirements from requirements.txt
 pip install -r requirements.txt
 # Collect static files (use appropriate settings)
-python manage.py collectstatic --noinput --clear 
+python manage.py collectstatic --noinput --clear --verbosity 0
