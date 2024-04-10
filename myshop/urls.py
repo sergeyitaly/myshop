07c90dd312fca0f832e1d59b087097652e05b2a2
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-11 00:22:36 +0300

    fix

--- myshop/urls.py
+++ myshop/urls.py
@@ -35,6 +35,5 @@ urlpatterns = [
     path('debug/', TemplateView.as_view(template_name='base.html'), name='debug'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
