142471dbf5c362d227bb22105c907ec8d073f403
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-10 15:32:02 +0300

    Update settings.py

--- myshop/settings.py
+++ myshop/settings.py
@@ -111,23 +111,18 @@ WSGI_APPLICATION = 'myshop.wsgi.app'
 #    }
 #}
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': os.environ["DB_NAME"],
-#        'USER': os.environ["DB_USER"],
-#        'PASSWORD': os.environ["DB_PASSWORD"],
-#        'HOST': os.environ["DB_HOST"],
-#        'PORT': os.environ["DB_PORT"],
-#    }
-#}
-
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': "django.db.backends.postgresql_psycopg2",
+        'NAME': os.environ["DB_NAME"],
+        'USER': os.environ["DB_USER"],
+        'PASSWORD': os.environ["DB_PASSWORD"],
+        'HOST': os.environ["DB_HOST"],
+        'PORT': os.environ["DB_PORT"],
+    }
 }
+
+
 STATIC_URL = '/static/'
 
 # List of directories where Django will look for static files
