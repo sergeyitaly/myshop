b230bf6ee30abe92a8603b4ff0f4e1cbab86432f
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-10 11:38:13 +0300

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,5 @@
-# build_files.sh
-python3.9 -m venv myenv  # Create a virtual environment named 'myenv'
-source myenv/bin/activate  # Activate the virtual environment
 
+
+python3.9 -m pip install --upgrade pip
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
