f81d2928c72876c8794e57fc49ed21903cce14c0
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-10 14:28:56 +0300

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,15 @@
-# build_files.sh
-mkdir -p /vercel/path0/static
+#!/bin/bash
 
+# Create and activate a virtual environment
+python3 -m venv myenv
+source myenv/bin/activate
+
+# Upgrade pip and install requirements
+python -m pip install --upgrade pip
 pip install -r requirements.txt
+
+# Set Django settings module
+export DJANGO_SETTINGS_MODULE=myshop.settings
+
+# Collect static files
 python manage.py collectstatic --noinput
