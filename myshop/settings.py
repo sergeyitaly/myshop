efd1c99eed76193ab6d7397b24d05f658445fd49
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-10 11:27:35 +0300

    Update settings.py

--- myshop/settings.py
+++ myshop/settings.py
@@ -15,7 +15,7 @@ if os.path.isfile(dotenv_file):
     
     
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ('SECRECT_KEY')
+SECRET_KEY = os.environ['SECRECT_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
