5ef338cc3ef795b1bed4183a5322bdd809206c49
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-10 12:28:27 +0300

    Update settings.py

--- myshop/settings.py
+++ myshop/settings.py
@@ -17,7 +17,7 @@ if os.path.isfile(dotenv_file):
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh"]
