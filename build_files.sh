74cf7172199c9c86add405f8ae5b557c9f85c34d
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-11 12:59:00 +0300

    build

--- build_files.sh
+++ build_files.sh
@@ -3,10 +3,6 @@
 # Create and activate a virtual environment named 'myenv'
 python3 -m venv myenv
 source myenv/bin/activate
-
-# Upgrade pip inside the virtual environment
-python -m pip install --upgrade pip
-
 # Install Python dependencies from requirements.txt
 pip install -r requirements.txt
 
@@ -24,5 +20,5 @@ python manage.py migrate
 export DJANGO_SETTINGS_MODULE=myshop.settings
 
 # Collect static files (use appropriate settings)
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput --clear
 
