0ede521236640853b1e030fc682aa39ebc627aa2
Kolotylo Kyrylo <kolot102@gmail.com>
2024-04-11 17:55:30 +0300

    feat: implement 404 page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,20 @@
 import { Route, Routes } from 'react-router-dom';
-import { Home } from './pages/home';
+import { Home } from './pages/home/home';
 import { Layout } from './layout/Layout/Layout';
+import { NotFound } from './pages/not-found/not-found';
 
 function App() {
     return (
         <>
             <Routes>
-                <Route element={<Layout withFooter withHeader/>}>
+                <Route
+                    element={
+                        <Layout
+                            withFooter
+                            withHeader
+                        />
+                    }
+                >
                     <Route
                         index
                         element={<Home />}
@@ -14,6 +22,10 @@ function App() {
                     <Route
                         path="collection"
                         element={<h1>Collection</h1>}
+                    />
+                    <Route
+                        path="*"
+                        element={<NotFound />}
                     />
                 </Route>
             </Routes>
