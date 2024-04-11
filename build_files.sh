b3e00dca07fb5ec1c09395ecc1d99bb05c9e2445
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-11 10:27:22 +0300

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -11,6 +11,12 @@ python -m pip install --upgrade pip
 pip install -r requirements.txt
 
 
+python manage.py makemigrations shop
+python manage.py makemigrations accounts
+python manage.py makemigrations cart
+python manage.py migrate shop
+python manage.py migrate account
+python manage.py migrate cart
 python manage.py makemigrations
 python manage.py migrate
 
