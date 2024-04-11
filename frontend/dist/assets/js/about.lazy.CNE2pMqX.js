b5f86ba2abd7e23fab575c4e5b10ad5d3907e129
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-12 01:49:30 +0300

    fix

---
+++ frontend/dist/assets/js/about.lazy.CNE2pMqX.js
@@ -0,0 +1,1 @@
+import{c as t,j as o}from"./main.BX1vg_v5.js";const r=t("/about")({component:e});function e(){return o.jsx("div",{children:o.jsx("h3",{children:"Welcome About!"})})}export{r as Route};
