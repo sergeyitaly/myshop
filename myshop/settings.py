c513fa57cfb4fd4fb4eb29477a75eff2dc9136d8
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-11 19:37:13 +0300

    settings

--- myshop/settings.py
+++ myshop/settings.py
@@ -116,7 +116,7 @@ DATABASES = {
 STATIC_URL = '/static/'
 # Directory where collected static files will be stored.
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 #STATICFILES_DIRS = [
 #    os.path.join(BASE_DIR, 'frontend', 'dist'),
