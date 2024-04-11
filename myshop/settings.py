f8183a4c8bea14ed2c320fb3a01651470c1f057c
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-11 13:43:59 +0300

    fix

--- myshop/settings.py
+++ myshop/settings.py
@@ -99,17 +99,19 @@ TEMPLATES = [
 #WSGI_APPLICATION = 'myshop.wsgi.application'
 WSGI_APPLICATION = 'myshop.wsgi.app'
 
-DATABASES = {
-    'default': {        
-       #'ENGINE': 'django.db.backends.postgresql_psycopg2',
-       'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ["POSTGRES_DATABASE"],
-        'USER': os.environ["POSTGRES_USER"],
-        'PASSWORD': os.environ["POSTGRES_PASSWORD"],
-        'HOST': os.environ["POSTGRES_HOST"],
-        'PORT': '5432',
-    }
-}
+#DATABASES = {
+#    'default': {        
+#       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#       'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': os.environ["POSTGRES_DATABASE"],
+#        'USER': os.environ["POSTGRES_USER"],
+#        'PASSWORD': os.environ["POSTGRES_PASSWORD"],
+#        'HOST': os.environ["POSTGRES_HOST"],
+#        'PORT': '5432',
+#    }
+#}
+DATABASES ={}
+
 STATIC_URL = '/static/'
 # Directory where collected static files will be stored.
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
