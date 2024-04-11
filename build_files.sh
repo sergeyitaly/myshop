3fa52ede9ec3f1e0f43c25ad0f03a2b9cd0278ef
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-11 22:35:13 +0300

    fix

--- build_files.sh
+++ build_files.sh
@@ -6,15 +6,6 @@ source myenv/bin/activate
 # Install Python dependencies from requirements.txt
 pip install -r requirements.txt
 
-python manage.py makemigrations shop
-python manage.py makemigrations accounts
-python manage.py makemigrations cart
-python manage.py migrate shop
-python manage.py migrate account
-python manage.py migrate cart
-python manage.py makemigrations
-python manage.py migrate
-
 # Set Django settings module (replace 'myshop.settings' with your actual settings module)
 export DJANGO_SETTINGS_MODULE=myshop.settings
 
