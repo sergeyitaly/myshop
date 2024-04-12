1b7108fa977458590acf01f9892533c1ea33a769
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-13 00:12:47 +0300

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,17 +1,11 @@
-pip install --upgrade pip
-# Install Python dependencies from requirements.txt
-pip install -r requirements.txt
+#!/bin/bash
+# Build the project
+echo "Building the project..."
+python3.9 -m pip install -r requirements.txt
 
-python manage.py makemigrations shop
-python manage.py makemigrations accounts
-python manage.py makemigrations cart
-python manage.py migrate shop
-python manage.py migrate account
-python manage.py migrate cart
-python manage.py makemigrations
-python manage.py migrate
+echo "Make Migration..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
 
-
-# Collect static files (use appropriate settings)
-python manage.py collectstatic --noinput --clear
-
+echo "Collect Static..."
+python3.9 manage.py collectstatic --noinput --clear
