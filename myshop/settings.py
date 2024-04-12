4ea330f594f6c6755484662b7900ef591426ee75
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-12 23:34:42 +0300

    AWS

--- myshop/settings.py
+++ myshop/settings.py
@@ -114,12 +114,12 @@ if STORAGE_DESTINATION == 's3':
     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-    AWS_DEFAULT_ACL = 'public-read'
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-    # static files settings
+    AWS_S3_CUSTOM_DOMAIN = f'{os.getenv(AWS_STORAGE_BUCKET_NAME)}.s3.amazonaws.com'
     AWS_LOCATION = 'static'
+    AWS_DEFAULT_ACL = 'public-read'
+    AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     # public media settings
@@ -128,12 +128,13 @@ if STORAGE_DESTINATION == 's3':
     DEFAULT_FILE_STORAGE = 'core.storage_backends.PublicMediaStorage'
 
 else:
-    STATIC_URL = "static/"
+    STATIC_URL = "/static/"
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
-    STAICFILES_DIRS =[os.path.join(BASE_DIR, 'static')]
 
-    MEDIA_ROOT = "media/"
-    MEDIA_URL = "media/"
+STAICFILES_DIRS =[os.path.join(BASE_DIR, 'static')]
+
+MEDIA_ROOT = MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = "/media/"
 
 #STATICFILES_FINDERS = [
 #    'django.contrib.staticfiles.finders.FileSystemFinder',
