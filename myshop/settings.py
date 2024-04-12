0ebb94f9ca51d34455c9c13651f04522cd3beda1
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-12 23:56:24 +0300

    vercel json

--- myshop/settings.py
+++ myshop/settings.py
@@ -108,33 +108,28 @@ DATABASES = {
 }
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
 
-STORAGE_DESTINATION = os.getenv('STORAGE_DESTINATION')
+USE_S3 = os.getenv('USE_S3') == 'True'
 
-if STORAGE_DESTINATION == 's3':
+if USE_S3:
+    # aws settings
     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-    AWS_S3_CUSTOM_DOMAIN = f'{os.getenv(AWS_STORAGE_BUCKET_NAME)}.s3.amazonaws.com'
-    AWS_LOCATION = 'static'
     AWS_DEFAULT_ACL = 'public-read'
-    AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    # s3 static settings
+    AWS_LOCATION = 'static'
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    # public media settings
-    PUBLIC_MEDIA_LOCATION = 'media'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
-    DEFAULT_FILE_STORAGE = 'core.storage_backends.PublicMediaStorage'
-
 else:
-    STATIC_URL = "/static/"
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
+    STATIC_URL = '/static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
-    STAICFILES_DIRS =[os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-    MEDIA_ROOT = MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-    MEDIA_URL = "/media/"
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 #STATICFILES_FINDERS = [
 #    'django.contrib.staticfiles.finders.FileSystemFinder',
