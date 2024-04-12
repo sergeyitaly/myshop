585b2ff38efaf4089746e115f5a2d6112c69caf8
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-12 21:39:21 +0300

    Update settings.py

--- myshop/settings.py
+++ myshop/settings.py
@@ -122,7 +122,6 @@ if STORAGE_DESTINATION == 's3':
     AWS_LOCATION = 'static'
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
     # public media settings
     PUBLIC_MEDIA_LOCATION = 'media'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
