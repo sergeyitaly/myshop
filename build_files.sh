7d764ca10d713781562d2ca633d68a36427c4279
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-14 00:57:52 +0300

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -5,7 +5,15 @@ python3 -m venv myenv
 source myenv/bin/activate
 # Install Python dependencies from requirements.txt
 pip install -r requirements.txt
-
+python3 -m pip uninstall python-dateutil --noinput
+python3 -m pip uninstall python3-openid --noinput
+python3 -m pip uninstall python-dateutil --noinput
+python3 -m pip uninstall jmespath --noinput
+python3 -m pip uninstall social-auth-app-django --noinput
+python3 -m pip uninstall urllib3 --noinput
+python3 -m pip uninstall six --noinput
+python3 -m pip uninstall idna --noinput
+python3 -m pip uninstall defusedxml --noinput
 # Set Django settings module
 #export DJANGO_SETTINGS_MODULE=myshop.settings
 
