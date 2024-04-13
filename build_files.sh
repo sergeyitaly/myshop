4abede1a1af3081478beec4e483186bc0f224cb4
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-13 13:14:45 +0300

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-# Build the project
-echo "Building the project..."
+# Create a virtual environment
+echo "Creating virtual environment..."
+python3.9 -m venv myenv
+
+# Activate the virtual environment
+echo "Activating virtual environment..."
+source myenv/bin/activate
 
 # Install required packages from requirements.txt
+echo "Installing required packages..."
 pip install -r requirements.txt
 
 # Make migrations and migrate database
 echo "Making migrations..."
-python3.9 manage.py makemigrations --noinput
+python manage.py makemigrations --noinput
 echo "Applying migrations..."
-python3.9 manage.py migrate --noinput
+python manage.py migrate --noinput
 
 # Collect static files
 echo "Collecting static files..."
-python3.9 manage.py collectstatic --clear --noinput
+python manage.py collectstatic --clear --noinput
+
+# Deactivate the virtual environment
+echo "Deactivating virtual environment..."
+deactivate
