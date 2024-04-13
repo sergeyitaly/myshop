c1a33b8e7e991d166b7a616dd3cd758a555194ae
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-14 01:24:58 +0300

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -5,12 +5,6 @@ python3 -m venv myenv
 source myenv/bin/activate
 # Install Python dependencies from requirements.txt
 pip install -r requirements.txt
-pip uninstall -y python3-openid
-pip uninstall -y django-node 
-pip uninstall -y social-auth-app-django
-pip uninstall -y six
-pip uninstall -y idna
-pip uninstall -y defusedxml
 # Set Django settings module
 #export DJANGO_SETTINGS_MODULE=myshop.settings
 
