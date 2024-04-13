8d22215f246d8956788c16d3f2624e6f6d15a7ab
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-13 14:45:23 +0300

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
-rm -rf ./*
-# Create a virtual environment
-echo "Creating virtual environment..."
-python3.9 -m venv myenv
 
-# Activate the virtual environment
-echo "Activating virtual environment..."
+# Change to project directory
+cd /vercel/path0
+
+# Activate virtual environment
 source myenv/bin/activate
 
-# Install required packages from requirements.txt
-echo "Installing required packages..."
+# Install or upgrade pip (optional)
+python -m pip install --upgrade pip
+
+# Install project dependencies
 pip install -r requirements.txt
 
-# Make migrations and migrate database
-echo "Making migrations..."
-python manage.py makemigrations --noinput
-echo "Applying migrations..."
-python manage.py migrate --noinput
+# Run Django migrations
+python manage.py makemigrations
+python manage.py migrate
 
 # Collect static files
-echo "Collecting static files..."
-python manage.py collectstatic --clear --noinput
+python manage.py collectstatic --no-input
 
-# Deactivate the virtual environment
-echo "Deactivating virtual environment..."
+# Deactivate virtual environment
 deactivate
