d60f0c1e97dccd5258a9548f65ca06780be523c6
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-13 21:15:54 +0300

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
+
+# Set up and activate virtual environment
 python3.9 -m venv myenv
 source myenv/bin/activate
+
 # Install Python dependencies from requirements.txt
 pip install -r requirements.txt
 
-# Set Django settings module (replace 'myshop.settings' with your actual settings module)
+# Set Django settings module
 export DJANGO_SETTINGS_MODULE=myshop.settings
 
-# Collect static files (use appropriate settings)
-python manage.py collectstatic --noinput --clear
+# Collect static files
+python3.9 manage.py collectstatic --noinput --clear
+
+# List contents and display disk usage
 ls -al
 du -h --max-depth=3 | sort -rh
 
-
+# Deactivate the virtual environment
+deactivate
