85013d6f768fd84cdadc092ce73cdbd704e969d7
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-13 10:46:16 +0300

    settings

--- myshop/settings.py
+++ myshop/settings.py
@@ -130,7 +130,7 @@ if USE_S3:
 
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    STATIC_ROOT = STATIC_URL
+    STATIC_ROOT = ''
 
     MEDIAFILES_LOCATION = "media"
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
