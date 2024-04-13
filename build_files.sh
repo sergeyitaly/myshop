4fe1547b6d6580fb2d639b64a0d977e7668653a1
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-13 14:13:02 +0300

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Create a virtual environment
-echo "Creating virtual environment..."
-python3.9 -m venv myenv
-
-# Activate the virtual environment
-echo "Activating virtual environment..."
-source myenv/bin/activate
-
 # Install required packages from requirements.txt
 echo "Installing required packages..."
 pip install -r requirements.txt
@@ -21,7 +13,3 @@ python manage.py migrate --noinput
 # Collect static files
 echo "Collecting static files..."
 python manage.py collectstatic --clear --noinput
-
-# Deactivate the virtual environment
-echo "Deactivating virtual environment..."
-deactivate
