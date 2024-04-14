823ebcf053a4f06fefbd2f51eefd3f6869aef251
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-14 14:39:10 +0300

    settings

--- shop/admin.py
+++ shop/admin.py
@@ -5,19 +5,14 @@ from django.conf import settings
 
 @admin.register(Collection)
 class CollectionAdmin(admin.ModelAdmin):
-    list_display = ['name', 'image_tag', 'photo']
+    list_display = ['name', 'image_tag']
     readonly_fields = ['image_tag']
 
     actions = ['delete_selected']
 
     def image_tag(self, obj):
         if obj.photo:
-            USE_S3 = settings.USE_S3 == 'True'  # Convert to boolean
-            if USE_S3:
-                photo_url = settings.MEDIA_URL + obj.photo.name
-            else:
-                photo_url = obj.photo.url
-            return format_html(f'<img src="{photo_url}" width="100" />')
+            return format_html('<img src="{}" style="max-width:200px; max-height:200px"/>'.format(obj.image.url))
         else:
             return '(No image)'
 
@@ -25,19 +20,14 @@ class CollectionAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'collection', 'image_tag', 'price', 'stock', 'available', 'photo')
+    list_display = ('name', 'collection', 'image_tag', 'price', 'stock', 'available')
     readonly_fields = ['image_tag']
 
     actions = ['delete_selected']
 
     def image_tag(self, obj):
         if obj.photo:
-            USE_S3 = settings.USE_S3 == 'True'  # Convert to boolean
-            if USE_S3:
-                photo_url = settings.MEDIA_URL + obj.photo.name
-            else:
-                photo_url = obj.photo.url
-            return format_html(f'<img src="{photo_url}" width="100" />')
+            return format_html('<img src="{}" style="max-width:200px; max-height:200px"/>'.format(obj.image.url))
         else:
             return '(No image)'
 
