7825f722213603711b2bcb9202fc4e9d93be868f
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-14 20:49:47 +0300

    settings

--- shop/admin.py
+++ shop/admin.py
@@ -1,45 +1,42 @@
 from django.contrib import admin
 from django.utils.html import format_html
-from django.urls import reverse
 from .models import Product, Collection
-from dotenv import load_dotenv
-import os
 from django.conf import settings
-
-load_dotenv()
 
 @admin.register(Collection)
 class CollectionAdmin(admin.ModelAdmin):
-    list_display = ['name', 'image_tag','photo']
+    list_display = ['name', 'image_tag', 'photo']
     readonly_fields = ['image_tag']
 
     actions = ['delete_selected']
 
     def image_tag(self, obj):
         if obj.photo:
-            USE_S3 = os.getenv('USE_S3') == 'TRUE'
-            if USE_S3:  photo_url = settings.MEDIA_URL + obj.photo.name
-            else:   photo_url = obj.photo.url
-            print(photo_url)
+            if settings.USE_S3:
+                photo_url = f'https://{settings.AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/{settings.AWS_MEDIA_LOCATION}/{obj.photo.name}'
+            else:
+                photo_url = obj.photo.url
             return format_html(f'<img src="{photo_url}" width="100" />')
         else:
-            return '(No image)'   
+            return '(No image)'
+
     image_tag.short_description = "Image"
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'collection', 'image_tag', 'price', 'stock', 'available','photo')
+    list_display = ('name', 'collection', 'image_tag', 'price', 'stock', 'available', 'photo')
     readonly_fields = ['image_tag']
 
     actions = ['delete_selected']
 
     def image_tag(self, obj):
         if obj.photo:
-            USE_S3 = os.getenv('USE_S3')=='TRUE'
-            if USE_S3:  photo_url = settings.MEDIA_URL + obj.photo.name
-            else:   photo_url = obj.photo.url
-            print(photo_url)
+            if settings.USE_S3:
+                photo_url = f'https://{settings.AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/{settings.AWS_MEDIA_LOCATION}/{obj.photo.name}'
+            else:
+                photo_url = obj.photo.url
             return format_html(f'<img src="{photo_url}" width="100" />')
         else:
-            return '(No image)'    
+            return '(No image)'
+
     image_tag.short_description = "Image"
