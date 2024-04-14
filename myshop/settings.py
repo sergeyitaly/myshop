b01b66afd2f0a94b898328394df86b65b59f7d40
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-14 17:56:18 +0300

    fix

--- myshop/settings.py
+++ myshop/settings.py
@@ -15,10 +15,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = True
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
+CORS_ALLOWED_ORIGINS = ['*']
 
 INTERNAL_IPS = [
     "127.0.0.1",
