2a449065c3e57a21f5f45c39ad9c038d72f6d3f4
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-16 00:21:25 +0300

    assets

--- build_files.sh
+++ build_files.sh
@@ -7,11 +7,14 @@
 pip install -r requirements.txt
 # Set Django settings module
 #export DJANGO_SETTINGS_MODULE=myshop.settings
+pip install awscli
+
+aws s3 sync templates s3://kolorytmedia/templates --delete
+aws s3 sync templates s3://kolorytmedia/dist --delete
+
 
 # Collect static files
 python3 manage.py collectstatic --noinput --clear
-pip install awscli
-aws s3 sync templates s3://kolorytmedia/templates --delete
 du -h --max-depth=5 | sort -rh
 
 # Deactivate the virtual environment
--- myshop/settings.py
+++ myshop/settings.py
@@ -129,6 +129,11 @@ if USE_S3:
     #DEFAULT_FILE_STORAGE ='storages.backends.s3boto3.S3Boto3Storage'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_MEDIA_LOCATION}/'
+    STATICFILES_DIRS = [
+    os.path.join(AWS_S3_CUSTOM_DOMAIN, 'dist'),  # Directory containing main.js and main.css
+    os.path.join(AWS_S3_CUSTOM_DOMAIN, 'dist', 'assets'),  # Directory containing other assets (images, etc.)
+    ]    
+
 
 else:
     # Local static file settings
@@ -139,15 +144,11 @@ else:
     STATIC_URL = '/static/'  # URL to serve static files
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
     WHITENOISE_ROOT = STATIC_ROOT
-
-
-
-
-# Static files settings (Vercel deployment)
-STATICFILES_DIRS = [
+    STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'dist'),  # Directory containing main.js and main.css
     os.path.join(BASE_DIR, 'dist', 'assets'),  # Directory containing other assets (images, etc.)
-]
+    ]
+
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
