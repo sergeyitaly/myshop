8b09aec5e25f5fd6f92a104575f7038f2aa53b65
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-17 01:15:49 +0300

    build

--- frontend/src/pages/CollectionPage/CollectionsPage.tsx
+++ frontend/src/pages/CollectionPage/CollectionsPage.tsx
@@ -1,42 +1,17 @@
 // CollectionsPage.tsx
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
-
 import { Link } from 'react-router-dom';
 import style from './style.module.scss';
-//import { fullData } from "../../components/Carousels/carouselMock";
-
-interface Collection {
-    id: number;
-    name: string;
-    category: string;
-    imageUrl: string;
-}
+import { fullData } from "../../components/Carousels/carouselMock";
 
 const CollectionsPage: React.FC = () => {
-    const [collections, setCollections] = useState<Collection[]>([]);
-
-    useEffect(() => {
-        const fetchCollections = async () => {
-            try {
-                const response = await axios.get('/api/collections/'); // Adjust endpoint URL
-                setCollections(response.data);
-            } catch (error) {
-                console.error('Error fetching collections:', error);
-            }
-        };
-
-        fetchCollections();
-    }, []);
-
     return (
         <div className={style.container}>
-            <h1 className={style.title}>Колекції</h1>
+            <h1 className={style.title}> Колекції </h1>
             <div className={style.cardContainer}>
-                {collections.map((collection) => (
+                {fullData.collections.map((collection) => (
                     <Link to={`/collections/${collection.id}`} key={collection.id} className={style.card}>
                         <div className={style.cardImage}>
-                            <img src={collection.imageUrl} alt={collection.name} style={{ maxWidth: '100%' }} />
+                            <img src={collection.imageUrl} alt={collection.name} style={{maxWidth:'100%'}} />
                             <p className={style.name}>{collection.name}</p>
                             <p className={style.category}>{collection.category}</p>
                         </div>
