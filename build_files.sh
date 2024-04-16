6e584ae97cff75c9a29b97d84c3470b02df8f22c
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-17 02:56:24 +0300

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
-#rm -rf ./*
+
 # Set up and activate virtual environment
-pip3 install wheel
-pip3 install virtualenv
-python3 -m venv venv
 #python3 -m venv myenv
-source myenv/bin/activate
+#source myenv/bin/activate
 # Install Python dependencies from requirements.txt
-
 pip install -r requirements.txt
-pip install --root-user-action=ignore
-
 # Set Django settings module
-export DJANGO_SETTINGS_MODULE=myshop.settings
-#pip install awscli
+#export DJANGO_SETTINGS_MODULE=myshop.settings
+pip install awscli
 
-#aws s3 sync templates s3://kolorytmedia/templates
-#aws s3 sync dist s3://kolorytmedia/dist
+aws s3 sync templates s3://kolorytmedia/templates
+aws s3 sync dist s3://kolorytmedia/dist
 
 # Collect static files
-python manage.py collectstatic --noinput --clear
-du -h -s
+python3 manage.py collectstatic --noinput --clear
+
 du -h --max-depth=5 | sort -rh
 
 # Deactivate the virtual environment
