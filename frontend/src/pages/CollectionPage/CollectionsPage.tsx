6b65a99b93c753b50c66afd4671f455ac05cba59
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-18 00:08:11 +0300

    daily commit

--- frontend/src/pages/CollectionPage/CollectionsPage.tsx
+++ frontend/src/pages/CollectionPage/CollectionsPage.tsx
@@ -1,25 +1,47 @@
-// CollectionsPage.tsx
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import style from './style.module.scss';
-import { fullData } from "../../components/Carousels/carouselMock";
 
-const CollectionsPage: React.FC = () => {
+interface Collection {
+    id: number;
+    slug: string;
+    name: string;
+    photo: string;
+}
+
+function CollectionsPage() {
+    const [collections, setCollections] = useState<Collection[]>([]);
+
+    useEffect(() => {
+        const fetchCollections = async () => {
+            try {
+                const response = await axios.get<Collection[]>('http://localhost:8000/collections/');
+                setCollections(response.data);
+            } catch (error) {
+                console.error('Error fetching collections:', error);
+            }
+        };
+
+        fetchCollections();
+    }, []);
+
+    let collection = collections.filter((obj) => obj.slug === "");
+    console.log(collection)
+
     return (
-        <div className={style.container}>
-            <h1 className={style.title}> Колекції </h1>
-            <div className={style.cardContainer}>
-                {fullData.collections.map((collection) => (
-                    <Link to={`/collections/${collection.id}`} key={collection.id} className={style.card}>
-                        <div className={style.cardImage}>
-                            <img src={collection.imageUrl} alt={collection.name} style={{maxWidth:'100%'}} />
-                            <p className={style.name}>{collection.name}</p>
-                            <p className={style.category}>{collection.category}</p>
-                        </div>
+        <Container>
+            <h1>Collections</h1>
+            {collections.map((collection) => (
+                <div key={collection.id}>
+                    <Link to={`/collections/${collection.slug}`}>
+                        <h3>{collection.name}</h3>
+                        <img src={collection.photo} alt={collection.name} style={{ maxWidth: '100%' }} />
                     </Link>
-                ))}
-            </div>
-        </div>
+                </div>
+            ))}
+        </Container>
     );
-};
+}
 
 export default CollectionsPage;
