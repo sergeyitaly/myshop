70879a664fc368d7935f3a4c5112e5ecfca82251
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-19 00:38:00 +0300

    models

--- shop/models.py
+++ shop/models.py
@@ -18,7 +18,7 @@ class MediaStorage(S3Boto3Storage):
     file_overwrite = True
     
 class Category(models.Model):
-    name = models.CharField(max_length=200, unique=True)
+    name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=200, unique=True, blank=True)
 
     def __str__(self):
@@ -36,8 +36,8 @@ class Collection(models.Model):
     else:    
         photo = models.ImageField(upload_to="photos/collection", null=True, blank=True)
 
-    name = models.CharField(max_length=200, db_index=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='collections')
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField(default=0)
     available = models.BooleanField(default=True)
