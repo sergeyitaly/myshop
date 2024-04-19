6e62712727c20336cc9704c29a34b5b9a84fd4fa
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-19 19:46:13 +0300

    dorenv

--- build_files.sh
+++ build_files.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # Set up and activate virtual environment
-python3 -m venv myenv
-source venv/bin/activate
+#python3 -m venv myenv
+#source venv/bin/activate
 # Install Python dependencies from requirements.txt
 #pip cache purge
 #rm -rf ../.cache/pip
 
 pip install -r requirements.txt
-#python3 manage.py makemigrations
-#python3 manage.py migrate
+python3 manage.py makemigrations
+python3 manage.py migrate
 # Set Django settings module
-export DJANGO_SETTINGS_MODULE=myshop.settings
+#export DJANGO_SETTINGS_MODULE=myshop.settings
 #pip install awscli
 
 #aws s3 sync templates s3://kolorytmedia/templates
@@ -20,4 +20,4 @@ export DJANGO_SETTINGS_MODULE=myshop.settings
 python3 manage.py collectstatic --noinput --clear
 du -h --max-depth=5 | sort -rh
 # Deactivate the virtual environment
-deactivate
+#deactivate
