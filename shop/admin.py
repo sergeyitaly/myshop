de0555d8cd14c85718ceb95c3e868383a777e101
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-20 11:08:21 +0300

    fix

--- shop/admin.py
+++ shop/admin.py
@@ -1,48 +1,58 @@
 from django.contrib import admin
-from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 from .models import Product, Collection, Category
-from django.utils.text import slugify  # Import slugify function
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    search_fields = ['name']  # Define fields to search on
-
+    search_fields = ['name']
     list_display = ['name', 'slug']
-    #readonly_fields = ['slug']
     actions = ['delete_selected']
 
 @admin.register(Collection)
 class CollectionAdmin(admin.ModelAdmin):
-    list_display = ['name', 'category','image_tag',  'price', 'stock', 'available','photo']
-    readonly_fields = ['image_tag']
+    list_display = ['id', 'name', 'category', 'image_thumbnail', 'price', 'stock', 'available', 'photo']
+    readonly_fields = ['image_thumbnail']
     actions = ['delete_selected']
-    list_filter = ('category','available',)
+    list_filter = ('category', 'available')
+    fields = ['name', 'category', 'price', 'stock', 'available', 'photo']  # Include 'photo' field here
 
-    def image_tag(self, obj):
+
+    def image_thumbnail(self, obj):
         if obj.photo:
-            photo_url = obj.photo.url
-            print(photo_url)  # For debugging
-            return format_html(f'<img src="{photo_url}" width="100" />')
+            return mark_safe(f'<img src="{obj.photo.url}" width="100" />')
         else:
             return '(No image)'
 
-    image_tag.short_description = "Image"
-    image_tag.allow_tags = True
+    image_thumbnail.short_description = "Image"
 
-    
+    def delete_selected(self, request, queryset):
+        for obj in queryset:
+            if obj.photo:
+                obj.photo.delete()
+            obj.delete()
+
+    delete_selected.short_description = "Delete selected collections"
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'image_tag', 'price', 'stock', 'available', 'photo')
-    readonly_fields = ['image_tag']
+    list_display = ('name', 'image_thumbnail', 'price', 'stock', 'available', 'photo')
+    readonly_fields = ['image_thumbnail']
     actions = ['delete_selected']
+    fields = ['name', 'price', 'stock', 'available', 'photo']  # Include 'photo' field here
 
-    def image_tag(self, obj):
+
+    def image_thumbnail(self, obj):
         if obj.photo:
-            photo_url = obj.photo.url
-            print(photo_url)  # For debugging
-            return format_html(f'<img src="{photo_url}" width="100" />')
+            return mark_safe(f'<img src="{obj.photo.url}" width="100" />')
         else:
             return '(No image)'
 
-    image_tag.short_description = "Image"
-    image_tag.allow_tags = True
+    image_thumbnail.short_description = "Image"
+
+    def delete_selected(self, request, queryset):
+        for obj in queryset:
+            if obj.photo:
+                obj.photo.delete()
+            obj.delete()
+
+    delete_selected.short_description = "Delete selected products"
