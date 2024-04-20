6542ec4543affd64ea7fdc1ff4c47a2efedef38a
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-21 00:50:55 +0300

    media move

--- build_files.sh
+++ build_files.sh
@@ -12,8 +12,8 @@ python3 manage.py makemigrations
 python3 manage.py migrate
 # Set Django settings module
 #export DJANGO_SETTINGS_MODULE=myshop.settings
-#pip install awscli
-#aws s3 mv staticfiles_build s3://kolorytmedia/staticfiles_build --recursive
+pip install awscli
+aws s3 mv media s3://kolorytmedia/media --recursive
 python3 manage.py collectstatic --noinput --clear
 du -h --max-depth=5 | sort -rh
 # Deactivate the virtual environment
