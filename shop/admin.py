2636895a236e9f7c38c2241c4bcb3ef1101706c9
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-20 22:24:24 +0300

    models

--- shop/admin.py
+++ shop/admin.py
@@ -14,59 +14,93 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Collection)
 class CollectionAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category','price', 'stock', 'available', 'created', 'updated', 'display_image')
-    readonly_fields = ['slug']
+    list_display = ('name', 'category','display_photo','display_description', 'display_price', 'stock', 'available')
+    readonly_fields = ['slug']  # Make slug readonly
 
     # Specify the fields to display and edit in the admin form
-    fieldsets = [
-        (None, {'fields': ['name', 'description', 'photo', 'price', 'stock', 'available']}),
-        ('Slug', {'fields': ['slug'], 'classes': ['collapse']}),
-    ]
+    fields = ['name', 'description', 'category','photo', 'price', 'currency', 'stock', 'available', 'slug']
+    
+    def display_price(self, obj):
+        return f'{obj.currency} {obj.price}'
 
-    # Prepopulate the slug field based on the name field
-    prepopulated_fields = {'slug': ('name',)}
-
+    display_price.short_description = 'Price'
+    
     def display_image(self, obj):
         if obj.photo:
-            return '<img src="{}" style="max-height: 150px; max-width: 150px;" />'.format(obj.photo.url)
+            return '<img src="{}" style="max-height: 50px; max-width: 50px;" />'.format(obj.photo.url)
         else:
             return 'No Image Found'
 
     display_image.short_description = 'Image'
     display_image.allow_tags = True  # Required for HTML rendering in Django admin
+    
+    def display_photo(self, obj):
+        if obj.photo:
+            return format_html('<img src="{}" style="max-height: 50px; max-width: 50px;" />'.format(obj.photo.url))
+        else:
+            return 'No Image Found'
 
+    display_photo.short_description = 'Photo Preview'
     def save_model(self, request, obj, form, change):
         """Automatically generate the slug when saving a new product."""
         if not obj.slug:  # Generate slug only if it's not set
             obj.slug = slugify(obj.name)
         super().save_model(request, obj, form, change)
 
+    def display_description(self, obj):
+        if obj.description:
+            return obj.description[:150] + '...' if len(obj.description) > 150 else obj.description
+        else:
+            return 'No Description'
+
+    display_description.short_description = 'Description'
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'stock', 'available', 'created', 'updated', 'display_image')
-    readonly_fields = ['slug']
+    list_display = ('name', 'display_photo','display_description','display_price', 'stock', 'available')
+    readonly_fields = ['slug']  # Make slug readonly
 
     # Specify the fields to display and edit in the admin form
-    fieldsets = [
-        (None, {'fields': ['name', 'description', 'photo', 'price', 'stock', 'available']}),
-        ('Slug', {'fields': ['slug'], 'classes': ['collapse']}),
-    ]
+    fields = ['name', 'description', 'photo', 'price', 'currency','stock', 'available', 'slug']
+    
+    def display_price(self, obj):
+        return f'{obj.currency} {obj.price}'
 
-    # Prepopulate the slug field based on the name field
-    prepopulated_fields = {'slug': ('name',)}
-
+    display_price.short_description = 'Price'
+    
+    def save_model(self, request, obj, form, change):
+        """Automatically generate the slug when saving a new product."""
+        if not obj.slug:
+            obj.slug = slugify(obj.name)
+        super().save_model(request, obj, form, change)
+        
     def display_image(self, obj):
         if obj.photo:
-            return '<img src="{}" style="max-height: 150px; max-width: 150px;" />'.format(obj.photo.url)
+            return '<img src="{}" style="max-height: 50px; max-width: 50px;" />'.format(obj.photo.url)
         else:
             return 'No Image Found'
 
     display_image.short_description = 'Image'
     display_image.allow_tags = True  # Required for HTML rendering in Django admin
 
+    def display_photo(self, obj):
+        if obj.photo:
+            return format_html('<img src="{}" style="max-height: 50px; max-width: 50px;" />'.format(obj.photo.url))
+        else:
+            return 'No Image Found'
+
+    display_photo.short_description = 'Photo Preview'
+    
     def save_model(self, request, obj, form, change):
         """Automatically generate the slug when saving a new product."""
         if not obj.slug:  # Generate slug only if it's not set
             obj.slug = slugify(obj.name)
         super().save_model(request, obj, form, change)
+        
+    def display_description(self, obj):
+        if obj.description:
+            return obj.description[:150] + '...' if len(obj.description) > 150 else obj.description
+        else:
+            return 'No Description'
+
+    display_description.short_description = 'Description'
