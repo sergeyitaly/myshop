e0686da1634852ec561eb235a4e221745f9300dd
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-21 15:11:11 +0300

    fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,22 @@
-import { Route, Routes } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Route, Routes} from 'react-router-dom';
 import { Layout } from './layout/Layout/Layout';
-import CollectionItemsPage from './pages/CollectionItem/CollectionItems';
 import CollectionsPage from './pages/CollectionPage/CollectionsPage';
+import CollectionItemsPage from './pages/CollectionItem/CollectionItems';
 import { Home } from './pages/home/home';
 import { NotFound } from './pages/not-found/not-found';
-import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-function App() {
-    const [collections, setCollections] = useState([]);
-//    const [collection, setCollection] = useState([]);
+interface Collection {
+    id: string;
+    name: string;
+    photo: string;
+    price: string;
+    category: string;
+}
 
+function App() {
+    const [collections, setCollections] = useState<Collection[]>([]);
     useEffect(() => {
         const fetchCollections = async () => {
             try {
@@ -21,17 +27,6 @@ function App() {
             }
         };
         fetchCollections();
-
-
-//        const fetchCollection = async () => {
-//            try {
-//                const response = await axios.get('/collections/:id/');
-//                setCollection(response.data);
-//            } catch (error) {
-//                console.error('Error fetching collection:', error);
- //           }
-//       };
-//        fetchCollection();
     }, []);
 
     return (
@@ -39,9 +34,7 @@ function App() {
             <Route element={<Layout withFooter withHeader />}>
                 <Route index element={<Home />} />
                 <Route path="/collections" element={<CollectionsPage collections={collections} />} />
-                <Route path="/collections/:id" element={<CollectionItemsPage />} 
-                //collection={collection} />}
-                />
+                <Route path="/collection/:id" element={<CollectionItemsPage />} />
                 <Route path="*" element={<NotFound />} />
             </Route>
         </Routes>
