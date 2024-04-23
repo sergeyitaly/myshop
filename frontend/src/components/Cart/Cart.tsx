b363d9678b961d82dea2dadc8cf1a2da1dbc97c4
Ostapenko Nataliia <nataliia.ostapenko@student.itmont.de>
2024-04-23 15:42:37 +0200

    add cart

--- frontend/src/components/Cart/Cart.tsx
+++ frontend/src/components/Cart/Cart.tsx
@@ -1,14 +1,31 @@
 import styles from './Cart.module.scss';
 import CartSVG from './cart.svg';
+import {useState} from "react";
+import CartModal from "./CartComponent";
 
 export const Cart = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleButtonClick = () => {
+        setIsModalOpen(true)
+    }
+
+    const closeModal = () => {
+      setIsModalOpen(false)
+    }
+
     return (
-        <button className={styles.button}>
-            <img
-                className={styles.icon}
-                src={CartSVG}
-                alt="Cart icon"
-            />
-        </button>
+        <>
+            <button className={styles.button}
+            onClick={handleButtonClick}>
+                <img
+                    className={styles.icon}
+                    src={CartSVG}
+                    alt="Cart icon"
+                />
+            </button>
+            {isModalOpen && <CartModal onClose={closeModal} />}
+        </>
+
     );
 };
