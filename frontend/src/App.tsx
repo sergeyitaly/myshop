f337d393ea63310f6d5f0acc960fb74eeaa1dda9
Matematuka <143654489+Matematuka@users.noreply.github.com>
2024-04-25 15:49:41 +0300

    product page images

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,42 +1,25 @@
-import { Route, Routes } from 'react-router-dom';
-import { Layout } from './layout/Layout/Layout';
-import CollectionItemsPage from './pages/CollectionItem/CollectionItems';
-import CollectionsPage from './pages/CollectionPage/CollectionsPage';
-import { Home } from './pages/home/home';
-import { NotFound } from './pages/not-found/not-found';
+import { Route, Routes } from "react-router-dom";
+import { Layout } from "./layout/Layout/Layout";
+import CollectionItemsPage from "./pages/CollectionItem/CollectionItems";
+import CollectionsPage from "./pages/CollectionPage/CollectionsPage";
+import { Home } from "./pages/home/home";
+import { NotFound } from "./pages/not-found/not-found";
+import ProductPage from "./pages/ProductPage/ProductPage";
 
 function App() {
-    return (
-        <>
-            <Routes>
-                <Route
-                    element={
-                        <Layout
-                            withFooter
-                            withHeader
-                        />
-                    }
-                >
-                    <Route
-                        index
-                        element={<Home />}
-                    />
-                    <Route
-                        path="collections"
-                        element={<CollectionsPage />}
-                    />
-                    <Route
-                        path="/collections/:id"
-                        element={<CollectionItemsPage />}
-                    />
-                    <Route
-                        path="*"
-                        element={<NotFound />}
-                    />
-                </Route>
-            </Routes>
-        </>
-    );
+  return (
+    <>
+      <Routes>
+        <Route element={<Layout withFooter withHeader />}>
+          <Route index element={<Home />} />
+          <Route path="collections" element={<CollectionsPage />} />
+          <Route path="/collections/:id" element={<CollectionItemsPage />} />
+          <Route path="/product/:id" element={<ProductPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
