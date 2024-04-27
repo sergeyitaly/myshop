324f446a4798dbeb6daebac24f1c4775bf137b25
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-27 15:11:40 +0300

    Update settings.py

--- myshop/settings.py
+++ myshop/settings.py
@@ -27,7 +27,6 @@ AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
 AWS_MEDIA_LOCATION = os.getenv('AWS_MEDIA', 'media')  # Default to 'media' if not specified
-AWS_LOCATION = 'staticfiles_build/'
 AWS_TEMPLATES =f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
 USE_S3 = bool(strtobool(os.getenv('USE_S3', 'True')))
 
@@ -139,7 +138,7 @@ if USE_S3:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' # new
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/staticfiles_build/'
     #DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     #STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_MEDIA_LOCATION}/'
