b699fa0384e9341c18dc22f2dc72847de86e06a2
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-27 15:35:23 +0300

    Update settings.py

--- myshop/settings.py
+++ myshop/settings.py
@@ -137,7 +137,7 @@ if USE_S3:
     #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/staticfiles_build/static/'
-    #DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_MEDIA_LOCATION}/'
 else:
