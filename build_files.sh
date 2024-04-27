b83572ed36833776464f90085ffbc486d4fecd19
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-04-27 18:39:59 +0300

    index html

--- build_files.sh
+++ build_files.sh
@@ -10,13 +10,10 @@
 pip install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
-
 # Set Django settings module
 #export DJANGO_SETTINGS_MODULE=myshop.settings
 pip install awscli
 aws s3 mv media s3://kolorytmedia/media --recursive
-#aws s3 mv staticfiles_build s3://kolorytmedia/staticfiles_build --recursive
 python3 manage.py collectstatic --noinput --clear
-
 du -h --max-depth=5 | sort -rh
 # Deactivate the virtual environment
