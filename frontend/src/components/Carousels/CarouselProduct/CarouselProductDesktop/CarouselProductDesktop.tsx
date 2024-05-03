3cdf7414fb638b94a1212d8dedc45dd809aca614
Matematuka <143654489+Matematuka@users.noreply.github.com>
2024-05-03 15:49:26 +0300

    descktopSlider

--- frontend/src/components/Carousels/CarouselProduct/CarouselProductDesktop/CarouselProductDesktop.tsx
+++ frontend/src/components/Carousels/CarouselProduct/CarouselProductDesktop/CarouselProductDesktop.tsx
@@ -1,38 +1,73 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import imgProductMax from "../../img/Rectangle 45.svg";
+import productImgMax from "../../img/Rectangle 45.svg";
+import ringQueen from "../../img/Rectangle 45.svg";
+import braceletQueen from "../../img/Rectangle 45.svg";
+import ringsSet from "../../img/Rectangle 45.svg";
+import earrings from "../../img/Rectangle 45.svg";
 import style from "./CarouselProductDesktop.module.scss";
 
-const CarouselProductMobile = () => {
+const CarouselProductDesktop = () => {
   const settings = {
+    customPaging: function (i) {
+      let imgSrc;
+      switch (i) {
+        case 0:
+          imgSrc = productImgMax;
+          break;
+        case 1:
+          imgSrc = ringQueen;
+          break;
+        case 2:
+          imgSrc = braceletQueen;
+          break;
+        case 3:
+          imgSrc = ringsSet;
+          break;
+        case 4:
+          imgSrc = earrings;
+          break;
+        default:
+          imgSrc = productImgMax;
+      }
+      return (
+        <div className={style.thumbnail}>
+          <img src={imgSrc} alt={`Thumbnail ${i}`} />
+        </div>
+      );
+    },
     dots: true,
-    lazyLoad: true,
+    dotsClass: "slick-dots slick-thumb",
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    initialSlide: 2,
-    variableWidth: false,
   };
+
   return (
-    <>
-      <Slider className={style.swiperContainer} {...settings}>
-        <div className={style.swiperSlide}>
-          <img className={style.imgMob} src={imgProductMax} alt="invida" />
-        </div>
-        <div className={style.swiperSlide}>
-          <img src={imgProductMax} alt="invida" />
-        </div>
-        <div className={style.swiperSlide}>
-          <img src={imgProductMax} alt="invida" />
-        </div>
-        <div className={style.swiperSlide}>
-          <img src={imgProductMax} alt="invida" />
-        </div>
-      </Slider>
-    </>
+    <div className={style.sliderContainer}>
+      <div className={style.slider}>
+        <Slider {...settings}>
+          <div>
+            <img src={productImgMax} alt="Product Image Max" />
+          </div>
+          <div>
+            <img src={ringQueen} alt="Ring Queen" />
+          </div>
+          <div>
+            <img src={braceletQueen} alt="Bracelet Queen" />
+          </div>
+          <div>
+            <img src={ringsSet} alt="Rings Set" />
+          </div>
+          <div>
+            <img src={earrings} alt="Earrings" />
+          </div>
+        </Slider>
+      </div>
+    </div>
   );
 };
 
-export default CarouselProductMobile;
+export default CarouselProductDesktop;
