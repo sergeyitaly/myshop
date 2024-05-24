5274583eb8e938f8d339f837c9f954aef229868c
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-05-24 22:20:39 +0300

    order page fix

--- order/serializers.py
+++ order/serializers.py
@@ -2,19 +2,34 @@ from rest_framework import serializers
 from .models import Order, OrderItem
 from shop.models import Product
 
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'price', 'collection']
+
 class OrderItemSerializer(serializers.ModelSerializer):
-    product = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all())  # Reference product by ID
+    product = ProductSerializer(read_only=True)
+    product_id = serializers.PrimaryKeyRelatedField(
+        source='product', 
+        queryset=Product.objects.all(), 
+        write_only=True
+    )  # Reference product by ID for creation
 
     class Meta:
         model = OrderItem
-        fields = ['product', 'quantity']
+        fields = ['product', 'product_id', 'quantity', 'price']
+
+    def create(self, validated_data):
+        product = validated_data.pop('product')
+        price = product.price
+        return OrderItem.objects.create(product=product, price=price, **validated_data)
 
 class OrderSerializer(serializers.ModelSerializer):
     items = OrderItemSerializer(many=True)
 
     class Meta:
         model = Order
-        fields = ['name', 'email', 'address', 'items']
+        fields = ['name', 'email', 'address', 'items', 'total_amount']
 
     def create(self, validated_data):
         items_data = validated_data.pop('items')
