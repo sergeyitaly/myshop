dcb094b1f94a872f24d9ff8d4978498c861a44b6
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-06-04 23:36:27 +0300

    Update admin.py

--- shop/admin.py
+++ shop/admin.py
@@ -16,7 +16,7 @@ class ProductImageInline(admin.StackedInline):
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     inlines = [ProductImageInline]
-    list_display = ('get_product_name', 'first_product_image', 'collection', 'price', 'currency', 'stock', 'available', 'sales_count', 'popularity')
+    list_display = ('id','get_product_name', 'first_product_image', 'collection', 'price', 'currency', 'stock', 'available', 'sales_count', 'popularity')
     search_fields = ['name']
     readonly_fields = ('id', 'slug', 'image_tag')
     fields = (
@@ -34,22 +34,20 @@ class ProductAdmin(admin.ModelAdmin):
     def first_product_image(self, obj):
         first_image = obj.productimage_set.first()  # Use the related manager
         if first_image and first_image.images:
-            print(first_image.images.url)  # Add this line to print the URL
-
             return format_html('<img src="{}" style="max-width:100px; max-height:100px;" />', first_image.images.url)
-        return 'No Image Found'
+        return format_html('<img src="{}" style="max-width:100px; max-height:100px;" />', obj.photo.url) if obj.photo else 'No Image Found'
     first_product_image.short_description = 'Image'
 
 @admin.register(ProductImage)
 class ProductImageAdmin(admin.ModelAdmin):
-    list_display = ['id', 'product', 'image_tag']
-    readonly_fields = ['image_tag']
+    list_display = ['id', 'product', 'get_image_url']
+    readonly_fields = ['get_image_url']
 
-    def image_tag(self, obj):
+    def get_image_url(self, obj):
         if obj.images:
-            return format_html('<img src="{}" style="max-width:100px; max-height:100px;" />', obj.images.url)
+            return obj.images.url
         return "No Image Found"
-    image_tag.short_description = 'Image'
+    get_image_url.short_description = 'Image URL'
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name',)
@@ -58,9 +56,13 @@ class CategoryAdmin(admin.ModelAdmin):
 admin.site.register(Category, CategoryAdmin)
 
 class CollectionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'category', 'image_tag')
+    list_display = ('id', 'get_collection_name_link', 'category', 'image_tag')
     search_fields = ['name']
     readonly_fields = ('id', 'image_tag')
+
+    def get_collection_name_link(self, obj):
+        return format_html('<a href="{}">{}</a>', reverse("admin:shop_collection_change", args=(obj.id,)), obj.name)
+    get_collection_name_link.short_description = "Collection"
 
     def image_tag(self, obj):
         if obj.photo:
