e96460510b02f9257ead92777e1ad4e55d185d2d
nalydrey <nalydrey@gmail.com>
2024-06-30 19:15:33 +0300

    hide form

--- frontend/src/pages/Order/OrderPage.tsx
+++ frontend/src/pages/Order/OrderPage.tsx
@@ -1,7 +1,6 @@
 import { PageContainer } from "../../components/PageContainer"
 import { OrderPreview } from "./OrderPreview/OrderPreview"
 import styles from './OrderPage.module.scss'
-import { OrderForm } from "../../components/Forms/OrderForm/OrderForm"
 
 
 
@@ -14,7 +13,7 @@ export const OrderPage = () => {
             <PageContainer>
                 <h1 className={styles.title}>Оформити замовлення</h1>
                 <section className={styles.orderSection}>
-                    <OrderForm className={styles.formContainer}/>
+                    {/* <OrderForm className={styles.formContainer}/> */}
                     <OrderPreview className={styles.preview}/>
                 </section>
             </PageContainer>
