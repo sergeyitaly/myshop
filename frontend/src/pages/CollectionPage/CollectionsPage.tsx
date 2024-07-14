765f74bbdb6ea0b98ec333d14422652ed062eae7
nalydrey <nalydrey@gmail.com>
2024-07-14 12:48:15 +0300

    return back pagination

--- frontend/src/pages/CollectionPage/CollectionsPage.tsx
+++ frontend/src/pages/CollectionPage/CollectionsPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Collection } from '../../models/entities';
 import { PreviewCard } from '../../components/Cards/PreviewCard/PreviewCard';
@@ -6,13 +6,20 @@ import { ROUTE } from '../../constants';
 import { useGetCollectionsByFilterQuery } from '../../api/collectionSlice';
 import { NamedSection } from '../../components/NamedSection/NamedSection';
 import { PreviewItemsContainer } from '../../components/containers/PreviewItemsContainer/PreviewItemsContainer';
+import { Pagination } from '../../components/UI/Pagination/Pagination';
+import styles from './style.module.scss'
 
 
 const CollectionsPage: React.FC = () => {
 
+  const limit = 9
+
+
   const navigate = useNavigate()
 
-  const {data, isLoading} = useGetCollectionsByFilterQuery({page_size: 20})
+  const [currentPage, setCurrentPage] = useState<number>(1)
+
+  const {data, isLoading, isFetching} = useGetCollectionsByFilterQuery({page_size: limit, page: currentPage})
 
   const collections: Collection[] = data?.results || []
 
@@ -20,6 +27,16 @@ const CollectionsPage: React.FC = () => {
     navigate(ROUTE.COLLECTION + id)
   }
 
+  let totalPages = 0
+
+  if(data){
+    totalPages = Math.ceil(data.count / limit)
+  }
+
+  const handleChangePage = (page: number ) => {
+    setCurrentPage(page)
+  }
+  
  
   return (
     <main>
@@ -35,12 +52,22 @@ const CollectionsPage: React.FC = () => {
                       key={collection.id}
                       photoSrc={collection.photo}
                       title={collection.name}
+                      loading={isFetching}
                       subTitle={collection.category}
                       onClick={() => handleClickCollectionCard(collection.id)}
                   />
                 ))
               }
           </PreviewItemsContainer>
+          {
+            data && totalPages > 1 &&
+            <Pagination
+              className={styles.pagination}
+              totalPages={totalPages}
+              currentPage={currentPage}
+              onChange = {handleChangePage}
+            />
+          }
         </NamedSection>
     </main>
   );
