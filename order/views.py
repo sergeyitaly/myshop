6648b70e25abc3d9b5e4dcb0cad1b38ba5ae3400
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-07-19 18:38:27 +0300

    Update views.py

--- order/views.py
+++ order/views.py
@@ -58,7 +58,7 @@ def create_order(request):
                     <td>{item.product.collection.name}</td>
                     <td>{item.quantity}</td>
                     <td>{item.product.size}</td>
-                    <td>{item.product.color_name}</td>
+                    <td>{item.product.color_name}<br>{item.product.color_value}</td>
                     <td>{item.product.price} {currency}</td>
                 </tr>
                 """
