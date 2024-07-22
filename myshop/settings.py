60c60dc41f26a0ef80d5738c697664732a15554a
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-07-22 18:38:28 +0300

    init

--- myshop/settings.py
+++ myshop/settings.py
@@ -74,6 +74,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'storages',
+    'imagekit',
     'drf_yasg',
     'django_filters',
     "phonenumber_field",
