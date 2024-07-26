b2655fed9ed661c118c31a7e2a2b3e89f7869765
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-07-26 16:57:02 +0300

    order urls and worker

--- order/urls.py
+++ order/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import OrderViewSet, create_order, telegram_webhook
+from .views import *
 
 # Create a router instance
 router = DefaultRouter()
 # Register viewsets with the router
 router.register(r'orders', OrderViewSet, basename='order')
+router.register(r'telegram_users', TelegramUserViewSet, basename='telegramuser')
 
 urlpatterns = [
     # Include all routes registered with the router
