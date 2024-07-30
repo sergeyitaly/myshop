341e7af31414627fba327dc2bae3522688563fac
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-07-31 01:24:30 +0300

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -3,9 +3,9 @@ cd frontend
 npm run vercel-build
 npm audit fix
 cd ..
-pip install -r requirements.txt
-python3 manage.py makemigrations
-python3 manage.py migrate
+ppip install -r requirements.txt
+ppython3 manage.py makemigrations
+ppython3 manage.py migrate
 pip install awscli
 aws s3 mv media s3://kolorytmedia/media --recursive
 python3 manage.py collectstatic --noinput --clear
