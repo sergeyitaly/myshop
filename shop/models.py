c48824fe01e48bf5e97e585661b632649ef63418
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-08-10 02:16:40 +0300

    admin model translation

--- shop/models.py
+++ shop/models.py
@@ -12,7 +12,7 @@ from imagekit.models import ImageSpecField
 from imagekit.processors import ResizeToFill
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-import django_filters
+from django.utils.translation import gettext_lazy as _
 
 
 load_dotenv()
@@ -50,8 +50,8 @@ class Category(models.Model):
 
     class Meta:
         ordering = ('name',)
-        verbose_name = 'Category'
-        verbose_name_plural = 'Categories'
+        verbose_name = _('Category')
+        verbose_name_plural = _('Categories')
 
 class Collection(models.Model):
     if USE_S3:
@@ -89,10 +89,10 @@ class Collection(models.Model):
 
     class Meta:
         ordering = ('name',)
-        verbose_name = 'Collection'
-        verbose_name_plural = 'Collections'
+        verbose_name = _('Collection')
+        verbose_name_plural = _('Collections')
 
-    image_tag.short_description = "Image"
+    image_tag.short_description =_("Image")
     image_tag.allow_tags = True
 
 class AdditionalField(models.Model):
@@ -125,21 +125,21 @@ class Product(models.Model):
         options={'quality': 60}
     )
 
-    collection = models.ForeignKey(Collection, on_delete=models.CASCADE, null=True, blank=True)
-    name = models.CharField(max_length=255)
-    description = models.TextField(null=True, blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.IntegerField(default=0)
-    available = models.BooleanField(default=True)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    sales_count = models.PositiveIntegerField(default=0)
-    popularity = models.PositiveIntegerField(default=0)
-    slug = models.SlugField(unique=True)
-    color_name = models.CharField(max_length=50, null=True, blank=True, help_text="Enter the color name, e.g., magenta or purple")
-    color_value = ColorField(default='#RRGGBB', null=True, blank=True, help_text="Enter the color value in the format #RRGGBB")
-    size = models.CharField(max_length=50, null=True, blank=True, help_text="Format: LxHxD (in mm or specify cm)")
-    discount = models.DecimalField(max_digits=5, decimal_places=2, default=0.00, null=True, blank=True, help_text="Discount percentage (e.g., 10.00 for 10%)")
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE, null=True, blank=True, verbose_name=_('Collection'))
+    name = models.CharField(max_length=255, verbose_name=_('Name'))
+    description = models.TextField(null=True, blank=True, verbose_name=_('Description'))
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name=_('Price'))
+    stock = models.IntegerField(default=0, verbose_name=_('Stock'))
+    available = models.BooleanField(default=True, verbose_name=_('Available'))
+    created = models.DateTimeField(auto_now_add=True, verbose_name=_('Created'))
+    updated = models.DateTimeField(auto_now=True, verbose_name=_('Updated'))
+    sales_count = models.PositiveIntegerField(default=0, verbose_name=_('Sales Count'))
+    popularity = models.PositiveIntegerField(default=0, verbose_name=_('Popularity'))
+    slug = models.SlugField(unique=True, verbose_name=_('Slug'))
+    color_name = models.CharField(max_length=50, null=True, blank=True, help_text=_("Enter the color name, e.g., magenta or purple"), verbose_name=_('Color Name'))
+    color_value = ColorField(default='#RRGGBB', null=True, blank=True, help_text=_("Enter the color value in the format #RRGGBB"), verbose_name=_('Color Value'))
+    size = models.CharField(max_length=50, null=True, blank=True, help_text=_("Format: LxHxD (in mm or specify cm)"), verbose_name=_('Size'))
+    discount = models.DecimalField(max_digits=5, decimal_places=2, default=0.00, null=True, blank=True, help_text=_("Discount percentage (e.g., 10.00 for 10%)"), verbose_name=_('Discount'))
 
     CURRENCY_CHOICES = (
         ('UAH', 'UAH (грн)'),
@@ -183,10 +183,10 @@ class Product(models.Model):
 
     class Meta:
         ordering = ('name',)
-        verbose_name = 'Product'
-        verbose_name_plural = 'Products'
+        verbose_name = _('Product')
+        verbose_name_plural = _('Products')
 
-    image_tag.short_description = "Image"
+    image_tag.short_description = _("Image")
     image_tag.allow_tags = True
 
 class ProductImage(models.Model):
