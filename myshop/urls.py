488165254f11ed1e23a6790ce64c2bd317b59c03
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-08-15 20:30:59 +0300

    swagger is fixed

--- myshop/urls.py
+++ myshop/urls.py
@@ -12,25 +12,22 @@ from django.views.generic import RedirectView
 from .views import CustomTokenObtainPairView, CustomTokenRefreshView
 from django.conf.urls.i18n import i18n_patterns
 from rest_framework_simplejwt.views import TokenRefreshView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from django.contrib import admin
+from dotenv import load_dotenv
+from drf_spectacular.views import SpectacularRedocView
 
 load_dotenv()
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="KOLORYT API",
-        default_version='v1',
-        description="Test description",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
 urlpatterns = [
-    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 
     # Authentication URLs
     path('auth/', include('djoser.urls')),
@@ -39,8 +36,9 @@ urlpatterns = [
 
     path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
     # App-specific URLs
-    path('api', include('accounts.urls')), 
+    path('api/', include('accounts.urls')), 
     path('api/', include('shop.urls')), 
     path('api/', include('order.urls')),
 
@@ -48,11 +46,7 @@ urlpatterns = [
     path('rosetta/', include('rosetta.urls')), 
 
     path("", views.index, name="index"),
-    # Catch-all URL pattern (redirect to index.html)
-   # re_path(r'^.*$', RedirectView.as_view(url='/')),
     path('i18n/', include('django.conf.urls.i18n')),
-
-    
 ]
 
 if settings.DEBUG:
@@ -62,7 +56,6 @@ if settings.DEBUG:
         urlpatterns += static('/assets/', document_root=settings.STATICFILES_DIRS[1])
 
 # Catch-all URL pattern for handling unmatched URLs
-#
 urlpatterns += [
     re_path(r'^.*$', RedirectView.as_view(url='/')),
 ]
