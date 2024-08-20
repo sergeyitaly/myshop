10d7a31d712d14a0b2a824002ce48873f9d76c5c
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-08-20 03:12:34 +0300

    entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,31 +10,31 @@ if [ -z "$SESSION_KEY" ]; then
 fi
 
 # Check if gunicorn_config.py exists
-if [ ! -f gunicorn_config.py ]; then
+#if [ ! -f gunicorn_config.py ]; then
     # If gunicorn_config.py does not exist, create it with basic settings
-    cat <<EOF > gunicorn_config.py
-bind = "0.0.0.0:8000"
-workers = 3
-accesslog = "-"
-errorlog = "-"
-loglevel = "info"
-EOF
-fi
+#    cat <<EOF > gunicorn_config.py
+#bind = "0.0.0.0:8000"
+#workers = 3
+#accesslog = "-"
+#errorlog = "-"
+#loglevel = "info"
+#EOF
+#fi
 
 # Create a temporary file with updated configuration
-TMP_FILE=$(mktemp)
+#TMP_FILE=$(mktemp)
 
 # Copy the existing configuration to the temporary file, excluding any old session_key entries
-grep -v '^session_key =' gunicorn_config.py > "$TMP_FILE"
+#grep -v '^session_key =' gunicorn_config.py > "$TMP_FILE"
 
 # Append the new session_key
-{
-    cat "$TMP_FILE"
-    echo "session_key = \"$SESSION_KEY\""
-} > gunicorn_config.py
+#{
+#    cat "$TMP_FILE"
+#    echo "session_key = \"$SESSION_KEY\""
+#} > gunicorn_config.py
 
 # Clean up the temporary file
-rm "$TMP_FILE"
+#rm "$TMP_FILE"
 
 # Export the session key as an environment variable
 export SESSION_KEY="$SESSION_KEY"
@@ -42,5 +42,5 @@ export SESSION_KEY="$SESSION_KEY"
 # Debug output to ensure the environment variable is set
 echo "SESSION_KEY: $SESSION_KEY"
 
-exec /app/venv/bin/gunicorn --config gunicorn_config.py myshop.wsgi:application
+#exec /app/venv/bin/gunicorn --config gunicorn_config.py myshop.wsgi:application
 
