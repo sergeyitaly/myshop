16fcca46a459783e2ef3ae5c858489866f476e08
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-08-21 14:26:13 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -45,7 +45,7 @@ echo "SESSION_KEY: $SESSION_KEY"
 
 # Clear the Django cache before making migrations
 echo "Clearing cache..."
-python manage.py db_cache_clean
+#python manage.py db_cache_clean
 
 exec /app/venv/bin/gunicorn --config gunicorn_config.py myshop.wsgi:application
 
