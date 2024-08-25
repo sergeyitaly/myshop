fb7b800d9954674affdd8ef4b861714cc4542ec5
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-08-25 21:47:54 +0300

    some optimization

--- myshop/settings.py
+++ myshop/settings.py
@@ -215,9 +215,19 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
+
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '10/minute',
+        'user': '100/minute'
+    },
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 8
 }
+
 
 SPECTACULAR_SETTINGS = {
     'TITLE': 'Your API',
