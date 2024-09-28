1efa2b2ba02857d6b1c2d10bf073569510190777
olyflower <2013.olga.yudina@gmail.com>
2024-09-28 16:24:14 +0300

    about - fix style, 404 - fix link

--- frontend/src/pages/not-found/not-found.tsx
+++ frontend/src/pages/not-found/not-found.tsx
@@ -1,22 +1,18 @@
-import styles from './not-found.module.scss';
-import Img404 from './404 error.png';
-import { useTranslation } from 'react-i18next'; // Import the useTranslation hook
-import { Link as RouterLink } from 'react-router-dom'; // Import RouterLink for navigation
+import { useTranslation } from "react-i18next"; 
+import { Link as RouterLink } from "react-router-dom"; 
+import Img404 from "./404 error.png";
+import styles from "./not-found.module.scss";
 
 export const NotFound = () => {
-    const { t } = useTranslation(); // Initialize translation hook
+	const { t } = useTranslation(); 
 
-    return (
-        <main className={styles.main}>
-            <img
-                className={styles.img}
-                src={Img404}
-                alt="404"
-            />
-            <h2 className={styles.message}>{t('not_found_message')}</h2>
-            <RouterLink to="/*" className={styles.link}>
-                {t('return_to_home')}
-            </RouterLink>
-        </main>
-    );
+	return (
+		<main className={styles.main}>
+			<img className={styles.img} src={Img404} alt="404" />
+			<h2 className={styles.message}>{t("not_found_message")}</h2>
+			<RouterLink to="/" className={styles.link}>
+				{t("return_to_home")}
+			</RouterLink>
+		</main>
+	);
 };
