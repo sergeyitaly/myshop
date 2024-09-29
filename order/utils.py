bae961ff539abaf4d9c8567bdda4181e740f0baf
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-09-30 00:28:24 +0300

    Update utils.py

--- order/utils.py
+++ order/utils.py
@@ -1,7 +1,7 @@
 from django.utils import timezone
 from order.models import Order
 from .notifications import update_order_status_with_notification
-from rest_framework.response import Response
+from django.utils.dateformat import format as date_format
 
 def update_order_statuses():
     now = timezone.now()
@@ -16,22 +16,33 @@ def update_orders(current_status, new_status, threshold_minutes, timestamp_field
         if order_timestamp and (now - order_timestamp).total_seconds() / 60 >= threshold_minutes:
             chat_id = order.telegram_user.chat_id if order.telegram_user else None
             if chat_id:
+                # Update the order status and corresponding timestamp
                 update_order_status(order, new_status, now, timestamp_field)
-                notify_user_with_order_status(order, new_status, chat_id)
+
+                # Notify user with the updated order summary
+                order_summary = prepare_order_summary(order, new_status)
+                update_order_status_with_notification(
+                    order.id,
+                    order_summary["order_items"],
+                    new_status,
+                    f'{new_status}_at',
+                    chat_id
+                )
 
 def update_order_status(order, new_status, now, timestamp_field):
     order.status = new_status
     setattr(order, f'{new_status}_at', now)  # Dynamically set the timestamp for the new status
     order.save()
 
-def notify_user_with_order_status(order, new_status, chat_id):
+def prepare_order_summary(order, new_status):
+    # Prepare order items data
     order_items_data = [
         {
             "size": item.size,
             "quantity": item.quantity,
             "total_sum": item.total_sum,
             "color_name": item.color_name,
-            "item_price": item.item_price,
+            "item_price": str(item.item_price),
             "color_value": item.color_value,
             "product_name": item.product_name,
             "collection_name": item.collection_name,
@@ -39,10 +50,15 @@ def notify_user_with_order_status(order, new_status, chat_id):
         for item in order.order_items.all()
     ]
     
-    update_order_status_with_notification(
-        order.id,
-        order_items_data,
-        new_status,
-        f'{new_status}_at',
-        chat_id
-    )
+    # Dynamically set the status field (processed_at, submitted_at, etc.)
+    status_timestamp_field = f'{new_status}_at'
+    status_timestamp = getattr(order, status_timestamp_field)
+    
+    # Prepare the order summary in the desired format
+    order_summary = {
+        "order_id": order.id,
+        "order_items": order_items_data,
+        status_timestamp_field: date_format(status_timestamp, 'Y-m-d H:i') if status_timestamp else None,
+    }
+    
+    return order_summary
