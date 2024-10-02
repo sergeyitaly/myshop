b68805f09bcf5d66c3bd2feaa153cdfc532b7bb8
olyflower <2013.olga.yudina@gmail.com>
2024-10-02 10:54:57 +0300

    about page-fix styles, product page-fix style, slider settings

--- frontend/src/components/ProductSection/ProductSection.tsx
+++ frontend/src/components/ProductSection/ProductSection.tsx
@@ -1,43 +1,41 @@
-import { PageContainer } from '../containers/PageContainer'
-import style from './ProducSection.module.scss'
-import { ProductGallery } from './components/ProductGallery/ProductGallery'
-import { ProductInfo } from './components/ProductInfo/ProductInfo'
-import { useParams } from 'react-router-dom'
-import { useProduct } from '../../hooks/useProduct'
-import { countDiscountPrice } from '../../functions/countDiscountPrice'
-
-
+import { PageContainer } from "../containers/PageContainer";
+import style from "./ProducSection.module.scss";
+import { ProductGallery } from "./components/ProductGallery/ProductGallery";
+import { ProductInfo } from "./components/ProductInfo/ProductInfo";
+import { useParams } from "react-router-dom";
+import { useProduct } from "../../hooks/useProduct";
+import { countDiscountPrice } from "../../functions/countDiscountPrice";
 
 export const ProductSection = () => {
+	const { id } = useParams<{ id: string }>();
 
-    const { id } = useParams<{ id: string }>();
+	const { product, variants, changeColor, changeSize } = useProduct(+id!);
 
-    const {product, variants, changeColor, changeSize} = useProduct(+id!)
+	const discountPrice = product
+		? countDiscountPrice(product.price, product.discount)
+		: null;
 
-    const discountPrice = product ? countDiscountPrice(product.price, product.discount) : null
-  
-    return (
-        <section>
-            <PageContainer className={style.aaa}>
-                {
-                product &&
-                    <>
-                        <ProductGallery
-                            defaultImage={product.photo_url}
-                            smallImg={product.photo_thumbnail_url}
-                            images={product.images}
-                            discount = {!!discountPrice}
-                        />
-                        <ProductInfo
-                            product={product}
-                            discountPrice={discountPrice}
-                            productVariants={variants}
-                            onChangeColor={changeColor}
-                            onChangeSize={changeSize}
-                        />
-                    </>
-                }
-            </PageContainer>
-        </section>
-    )
-}
+	return (
+		<section>
+			<PageContainer className={style.aaa}>
+				{product && (
+					<>
+						<ProductGallery
+							defaultImage={product.photo_url}
+							smallImg={product.photo_thumbnail_url}
+							images={product.images}
+							discount={!!discountPrice}
+						/>
+						<ProductInfo
+							product={product}
+							discountPrice={discountPrice}
+							productVariants={variants}
+							onChangeColor={changeColor}
+							onChangeSize={changeSize}
+						/>
+					</>
+				)}
+			</PageContainer>
+		</section>
+	);
+};
--- frontend/src/pages/ProductPage/ProductPage.tsx
+++ frontend/src/pages/ProductPage/ProductPage.tsx
@@ -1,44 +1,38 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import { ProductSection } from '../../components/ProductSection/ProductSection'; 
-import { MainContainer } from './components/MainContainer';
-import { useProduct } from '../../hooks/useProduct';
-import { useGetCollectionByNameQuery } from '../../api/collectionSlice';
-import { skipToken } from '@reduxjs/toolkit/query';
-import { useTranslation } from 'react-i18next';
-import { FromThisCollectionSection } from '../../sections/FromThisCollectionSection/FromthisCollectionSection';
-
-
+import React from "react";
+import { useParams } from "react-router-dom";
+import { ProductSection } from "../../components/ProductSection/ProductSection";
+import { MainContainer } from "./components/MainContainer";
+import { useProduct } from "../../hooks/useProduct";
+import { useGetCollectionByNameQuery } from "../../api/collectionSlice";
+import { skipToken } from "@reduxjs/toolkit/query";
+import { useTranslation } from "react-i18next";
+import { FromThisCollectionSection } from "../../sections/FromThisCollectionSection/FromthisCollectionSection";
 
 const ProductPage: React.FC = () => {
+	const { id } = useParams<{ id: string }>();
 
-  const { id } = useParams<{ id: string }>();
-  
-  const { t } = useTranslation();  // Hook for translation
+	const { t } = useTranslation();
 
-  const { product, isLoading, isFetching } = useProduct(+id!);
-  // Assuming `product.collection` is of type `Collection`
-  const collectionName = product?.collection?.name ?? skipToken;
+	const { product, isLoading, isFetching } = useProduct(+id!);
 
-  const { data: collection } = useGetCollectionByNameQuery(collectionName);
+	const collectionName = product?.collection?.name ?? skipToken;
 
+	const { data: collection } = useGetCollectionByNameQuery(collectionName);
 
-  if (isLoading) {
-    return <div>{t('loading')}</div>;  // Localized 'Loading...' text
-  }
+	if (isLoading) {
+		return <div>{t("loading")}</div>;
+	}
 
-  if (!product) {
-    return <div>{t('product_not_found')}</div>;  // Localized 'Product not found' text
-  }
+	if (!product) {
+		return <div>{t("product_not_found")}</div>;
+	}
 
-  return (
-    <MainContainer isLoading={isFetching}>
-      <ProductSection />
-      <FromThisCollectionSection
-        collectionId={collection?.id}
-      />
-    </MainContainer>
-  );
+	return (
+		<MainContainer isLoading={isFetching}>
+			<ProductSection />
+			<FromThisCollectionSection collectionId={collection?.id} />
+		</MainContainer>
+	);
 };
 
 export default ProductPage;
