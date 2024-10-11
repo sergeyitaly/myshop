846f83359a40d646d9c6d13324a244c78b4af114
olyflower <2013.olga.yudina@gmail.com>
2024-10-11 21:24:09 +0300

    fix bugs

--- frontend/src/pages/Order/OrderPreview/OrderPreview.tsx
+++ frontend/src/pages/Order/OrderPreview/OrderPreview.tsx
@@ -1,57 +1,55 @@
-import clsx from 'clsx'
-import { useBasket } from '../../../hooks/useBasket'
-import styles from './OrderPreview.module.scss'
-import { formatPrice } from '../../../functions/formatPrice'
-import { OrderItemCard } from '../../../components/Cards/OrderItemCard/OrderItemCard'
-import { ROUTE } from '../../../constants'
-import { Product } from '../../../models/entities'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
+import clsx from "clsx";
+import { useBasket } from "../../../hooks/useBasket";
+import { formatPrice } from "../../../functions/formatPrice";
+import { OrderItemCard } from "../../../components/Cards/OrderItemCard/OrderItemCard";
+import { ROUTE } from "../../../constants";
+import { Product } from "../../../models/entities";
+import { useTranslation } from "react-i18next";
+import styles from "./OrderPreview.module.scss";
 
 interface OrderPreviewProps {
-    className?: string
+	className?: string;
 }
 
+export const OrderPreview = ({ className }: OrderPreviewProps) => {
+	const navigate = useNavigate();
+	const { t } = useTranslation();
 
+	const { basketItems, totalPrice, deleteFromBasket, changeCounter } =
+		useBasket();
 
-export const OrderPreview = ({
-    className
-}: OrderPreviewProps) => {
+	const handleClickCard = (product: Product) => {
+		navigate(ROUTE.PRODUCT + product.id);
+	};
 
-    const navigate = useNavigate()
-
-    const { basketItems, totalPrice, deleteFromBasket, changeCounter} = useBasket()
-
-    const handleClickCard = (product: Product) => {
-        navigate(ROUTE.PRODUCT+product.id)
-    }
-
-
-    return (
-        <div className={clsx(className, styles.container )}>
-            <div className={styles.spaceBetween}>
-                <h2 className={styles.text}>Замовлення</h2>
-            </div>
-            <div className={styles.content}>
-            {
-                basketItems.map(({product, qty}) => (
-                    product &&
-                    <OrderItemCard 
-                        key={product.id}
-                        product={product}
-                        qty={qty}
-                        onClickDelete={deleteFromBasket}
-                        onChangeCounter={(val) => changeCounter(product, val)}
-                        onClickName={handleClickCard}
-                        onClickPhoto={handleClickCard}
-                    />
-                ))
-            }
-
-            </div>
-            <div className={styles.spaceBetween}>
-                <p className={styles.text}>Загальна сума</p>
-                <p className={styles.text}>{formatPrice(totalPrice, 'UAH')}</p>
-            </div>
-        </div>
-    )
-}
+	return (
+		<div className={clsx(className, styles.container)}>
+			<div className={styles.spaceBetween}>
+				<h2 className={styles.text}>{t("order")}</h2>
+			</div>
+			<div className={styles.content}>
+				{basketItems.map(
+					({ product, qty }) =>
+						product && (
+							<OrderItemCard
+								key={product.id}
+								product={product}
+								qty={qty}
+								onClickDelete={deleteFromBasket}
+								onChangeCounter={(val) =>
+									changeCounter(product, val)
+								}
+								onClickName={handleClickCard}
+								onClickPhoto={handleClickCard}
+							/>
+						)
+				)}
+			</div>
+			<div className={styles.spaceBetween}>
+				<p className={styles.text}>{t("total_price")}</p>
+				<p className={styles.text}>{formatPrice(totalPrice, "UAH")}</p>
+			</div>
+		</div>
+	);
+};
--- frontend/src/pages/ThankPage/ThankPage.tsx
+++ frontend/src/pages/ThankPage/ThankPage.tsx
@@ -1,59 +1,72 @@
-import { PageContainer } from '../../components/containers/PageContainer';
-import styles from './ThankPage.module.scss';
-import img from '../../assets/thank picture.svg';
-import { Link } from 'react-router-dom';
-import { ROUTE } from '../../constants';
-import { useMediaQuery } from 'react-responsive';
-import QRCode from 'qrcode.react';
-import { FaTelegramPlane } from 'react-icons/fa'; // Import Telegram icon
-import { useTranslation } from 'react-i18next'; // Import useTranslation
+import { PageContainer } from "../../components/containers/PageContainer";
+import img from "../../assets/thank picture.svg";
+import { Link } from "react-router-dom";
+import { ROUTE } from "../../constants";
+import { useMediaQuery } from "react-responsive";
+import QRCode from "qrcode.react";
+import { FaTelegramPlane } from "react-icons/fa";
+import { useTranslation } from "react-i18next";
+import styles from "./ThankPage.module.scss";
 
 export const ThankPage = () => {
-    const { t } = useTranslation(); // Use useTranslation hook
-    const isLaptop = useMediaQuery({ query: '(min-width: 1024px)' });
-    const telegramLink = "https://t.me/KOLORYT_notifications_bot";
+	const { t } = useTranslation();
+	const isLaptop = useMediaQuery({ query: "(min-width: 1024px)" });
+	const telegramLink = "https://t.me/KOLORYT_notifications_bot";
 
-    return (
-        <main>
-            <section>
-                <PageContainer className={styles.wrapper}>
-                    <div className={styles.container}>
-                        <h1 className={styles.title}>{t('thank_you')}</h1> {/* Localization */}
-                        <p className={styles.p}>
-                            {t('thank_you_message')} {/* Localization */}
-                        </p>
-                        <img className={styles.image} src={img} alt={t('thank_you_image_alt')} /> {/* Localization */}
-                        <p className={styles.check}>{t('check_email')}</p> {/* Localization */}
-                        <p className={styles.text}>
-                            {t('contact_us')} 
-                            <span className={styles.email}> {t('emailKOLORYT')}</span>
-                        </p>
-                        <div className={styles.telegramContainer}>
-                            <FaTelegramPlane size={24}
-                                             // className={styles.telegramIcon}
-                            />
-                            <p>
-                                <a href={telegramLink} target="_blank" rel="noopener noreferrer">
-                                    <span className={styles.KOLORYT_notifications_bot}>{t('telegram_bot')}</span>
-                                </a>
-                            </p>
-                        </div>
-                        {isLaptop && (
-                            <div className={styles.qrCode}>
-                                <p>{t('scan_qr_code')}</p>
-                                <br/>
-                                <QRCode value={telegramLink} size={128} />
-                            </div>
-                        )}
-                        <Link
-                            to={ROUTE.HOME}
-                            className={styles.link}
-                        >
-                            {t('go_home')} {/* Localization */}
-                        </Link>
-                    </div>
-                </PageContainer>
-            </section>
-        </main>
-    );
-}
+	return (
+		<main>
+			<section>
+				<PageContainer className={styles.wrapper}>
+					<div className={styles.container}>
+						<h1 className={styles.title}>{t("thank_you")}</h1>{" "}
+						<p className={styles.p}>{t("thank_you_message")}</p>
+						<img
+							className={styles.image}
+							src={img}
+							alt={t("thank_you_image_alt")}
+						/>{" "}
+						<p className={styles.check}>{t("check_email")}</p>{" "}
+						<p className={styles.text}>
+							{t("contact_us")}
+							<span className={styles.email}>
+								{" "}
+								{t("emailKOLORYT")}
+							</span>
+						</p>
+						<div className={styles.telegramContainer}>
+							<FaTelegramPlane
+								size={24}
+								// className={styles.telegramIcon}
+							/>
+							<p>
+								<a
+									href={telegramLink}
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									<span
+										className={
+											styles.KOLORYT_notifications_bot
+										}
+									>
+										{t("telegram_bot")}
+									</span>
+								</a>
+							</p>
+						</div>
+						{isLaptop && (
+							<div className={styles.qrCode}>
+								<p>{t("scan_qr_code")}</p>
+								<br />
+								<QRCode value={telegramLink} size={128} />
+							</div>
+						)}
+						<Link to={ROUTE.HOME} className={styles.link}>
+							{t("go_home")} {/* Localization */}
+						</Link>
+					</div>
+				</PageContainer>
+			</section>
+		</main>
+	);
+};
