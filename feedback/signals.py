f6b27798a34c9a9ef91269c3da3be273335ede91
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-10-23 00:45:38 +0300

    Feeadback update

--- feedback/signals.py
+++ feedback/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_save, post_delete, pre_delete
 from django.dispatch import receiver
 from django.db.models import Avg
 from django.db import transaction
@@ -6,10 +6,16 @@ from .models import RatingAnswer, OverallAverageRating, RatingQuestion
 
 def update_average_for_question(question):
     """Helper function to update average rating for a given question."""
+    if not question.rating_required:
+        return OverallAverageRating.objects.filter(question=question).delete()  # Optionally delete if not required
+
     avg_rating = RatingAnswer.objects.filter(question=question).aggregate(average=Avg('rating'))['average'] or 0
     overall_avg, created = OverallAverageRating.objects.get_or_create(question=question)
+    avg_rating = round(avg_rating, 1)
     overall_avg.average_rating = avg_rating
+    overall_avg.save()
     return overall_avg
+
 
 @receiver(post_save, sender=RatingAnswer)
 def update_average_rating(sender, instance, **kwargs):
@@ -18,7 +24,8 @@ def update_average_rating(sender, instance, **kwargs):
         overall_avgs = []  # List to hold updated OverallAverageRating instances
         for question in RatingQuestion.objects.all():
             overall_avg = update_average_for_question(question)
-            overall_avgs.append(overall_avg)  # Append to the updates list
+            if overall_avg is not None:
+                overall_avgs.append(overall_avg)  # Append to the updates list
 
         OverallAverageRating.objects.bulk_update(overall_avgs, ['average_rating'])  # Efficiently update all at once
 
@@ -29,6 +36,16 @@ def update_average_rating_on_delete(sender, instance, **kwargs):
         overall_avgs = []  # List to hold updated OverallAverageRating instances
         for question in RatingQuestion.objects.all():
             overall_avg = update_average_for_question(question)
-            overall_avgs.append(overall_avg)  # Append to the updates list
+            if overall_avg is not None:
+                overall_avgs.append(overall_avg)  # Append to the updates list
 
         OverallAverageRating.objects.bulk_update(overall_avgs, ['average_rating'])  # Efficiently update all at once
+
+@receiver(pre_delete, sender=RatingQuestion)
+def delete_related_average_rating(sender, instance, **kwargs):
+    with transaction.atomic():
+        OverallAverageRating.objects.filter(question=instance).delete()
+
+@receiver(post_delete, sender=RatingQuestion)
+def delete_overall_average_ratings(sender, instance, **kwargs):
+    OverallAverageRating.objects.filter(question=instance).delete()
