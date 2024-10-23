01d0b2fbb0f2b6706a9cb3d29ff73c5939768336
sergeyitaly <96667927+sergeyitaly@users.noreply.github.com>
2024-10-23 03:29:57 +0300

    Feeadbck admin fix

--- feedback/admin.py
+++ feedback/admin.py
@@ -96,6 +96,7 @@ class OverallAverageRatingAdmin(SortableAdminMixin,admin.ModelAdmin):
             rating_question_id = overall_avg_rating.question.id
             return HttpResponseRedirect(reverse('admin:feedback_ratingquestion_change', args=[rating_question_id]))       
         return super().change_view(request, object_id, form_url, extra_context)    
+
 def register_translation_admin(model, admin_class):
     try:
         admin.site.register(model, admin_class)
