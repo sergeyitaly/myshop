b42a73ff8eb5d3957f57a2bbfb65454f8b0e6b50
Maryna-Korbet <m22v86k@gmail.com>
2024-10-29 09:52:43 -0400

    connecting BurgerMenu pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,6 +17,9 @@ import {Contact} from "./pages/Contact/Contact";
 import {ThankYouPage} from "./pages/Contact/ThankYouPage/ThankYouPage";
 import { About } from './pages/About/About';
 import { FeedbackPage } from './pages/Feedback/Feedback';
+import { NewProducts } from './sections/TabSection/NewProducts';
+import { AllCollections } from './sections/TabSection/AllCollections';
+import { ProductsWithDiscount } from './sections/TabSection/ProductsWithDiscount';
 
 function App() {
 
@@ -39,6 +42,9 @@ function App() {
         <Route path={ROUTE.PAYMENT_DELIVERY} element={<PaymentAndDeliveryPage />} />
         <Route path={ROUTE.PRIVACY_POLICY} element={<PrivacyPolicyPage />} />
         <Route path={ROUTE.RETURNS_REFUNDS} element={<ReturnsRefundsPage />} />
+        <Route path={ROUTE.NEW_ARRIVALS} element={<NewProducts />} />
+        <Route path={ROUTE.ALL_COLLECTIONS} element={<AllCollections />} />
+        <Route path={ROUTE.DISCOUNT} element={<ProductsWithDiscount />} />
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
