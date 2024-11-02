69ac82afbda0ea462effcb7a989dacb71b419950
sergeyitaly <sergeyitaly@yahoo.com>
2024-11-02 23:54:06 +0200

    Feedback form

--- feedback/forms.py
+++ feedback/forms.py
@@ -3,25 +3,19 @@ from .models import Feedback, RatingAnswer, RatingQuestion
 
 
 class FeedbackForm(forms.ModelForm):
-    """
-    Form for collecting feedback details and dynamically adding rating fields based on RatingQuestion.
-    """
     class Meta:
         model = Feedback
-        fields = ['name', 'email', 'comment']  # Feedback basic fields
+        fields = ['name', 'email', 'comment']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-        # Dynamically add rating fields for each RatingQuestion, fetching them once
         self.questions = RatingQuestion.objects.all()
-
         for question in self.questions:
             self.fields[f'question_{question.id}_rating'] = forms.IntegerField(
                 label=question.aspect_name,
                 min_value=1,
                 max_value=10,
-                required=True,
+                required=question.rating_required,
                 widget=forms.NumberInput(attrs={'class': 'rating-input'}),
             )
             self.fields[f'question_{question.id}_answer'] = forms.CharField(
@@ -30,24 +24,25 @@ class FeedbackForm(forms.ModelForm):
                 widget=forms.Textarea(attrs={'class': 'answer-input', 'rows': 2}),
             )
 
+    def clean(self):
+        cleaned_data = super().clean()
+        for question in self.questions:
+            if question.rating_required:
+                rating_value = cleaned_data.get(f'question_{question.id}_rating')
+                if rating_value is None:
+                    self.add_error(f'question_{question.id}_rating', "This field is required.")
+        return cleaned_data
+
     def save(self, commit=True):
-        # Save the Feedback instance first
         feedback = super().save(commit=commit)
-
-        # Prepare list to bulk create or update RatingAnswers
         rating_answers = []
-
         for question in self.questions:
             rating_value = self.cleaned_data.get(f'question_{question.id}_rating')
             answer_value = self.cleaned_data.get(f'question_{question.id}_answer')
-
-            if rating_value is not None:  # Check if rating_value is provided
+            if rating_value is not None:
                 rating_answers.append(
                     RatingAnswer(feedback=feedback, question=question, rating=rating_value, answer=answer_value)
                 )
-
-        # Bulk create or update RatingAnswers
         if rating_answers:
             RatingAnswer.objects.bulk_create(rating_answers)
-
         return feedback
