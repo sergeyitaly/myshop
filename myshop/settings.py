bc10626b5ebdfb1c37c49ee60405858f037f3ddf
sergeyitaly <sergeyitaly@yahoo.com>
2024-11-03 20:18:57 +0200

    Redis timeout

--- myshop/settings.py
+++ myshop/settings.py
@@ -343,7 +343,7 @@ MODELTRANSLATION_FALLBACK_LANGUAGES = ('en','uk',)
 # }
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-REDIS_CACHE_LOCATION = os.getenv('REDIS_CACHE_LOCATION', 'redis://localhost:6379/1')
+REDIS_CACHE_LOCATION = os.getenv('REDIS_CACHE_LOCATION')
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
@@ -363,7 +363,7 @@ CACHES = {
 }
 
 # Celery configuration
-CELERY_BROKER_URL = os.getenv('REDIS_BROKER_URL', 'redis://localhost:6379/0')
+CELERY_BROKER_URL = os.getenv('REDIS_BROKER_URL')
 CELERY_RESULT_BACKEND = CELERY_BROKER_URL
 
 CELERY_IMPORTS = ('order.tasks',)
