84df1fc9e679922ea4a3a182331b840d8992b724
Ostapenko Nataliia <nataliia.ostapenko@student.itmont.de>
2024-11-07 19:44:07 +0100

    change basket, contact page

--- frontend/src/components/Cards/BasketItem/BasketItem.tsx
+++ frontend/src/components/Cards/BasketItem/BasketItem.tsx
@@ -4,11 +4,9 @@ import { countDiscountPrice } from "../../../functions/countDiscountPrice";
 import { formatPrice } from "../../../functions/formatPrice";
 import { Color, Product } from "../../../models/entities";
 import { AppImage } from "../../AppImage/AppImage";
-import { AvailableLable } from "../../AvailableLabel/AvailableLabel";
 import { Counter } from "../../Counter/Counter";
 import { Plug } from "../../Plug/Plug";
 import { ProductVariants } from "../../ProductVariants/ProductVariants";
-import { ValueBox } from "../../ProductVariants/ValueBox/ValueBox";
 import { IconButton } from "../../UI/IconButton/IconButton";
 import styles from "./BasketItem.module.scss";
 
@@ -25,7 +23,6 @@ interface BasketItemProps {
 
 export const BasketItem = ({
 	product,
-	color,
 	size,
 	qty,
 	onClickDelete,
@@ -37,7 +34,7 @@ export const BasketItem = ({
 	const { getTranslatedProductName, getTranslatedColorName } =
 		useAppTranslator();
 
-	const { photo, photo_thumbnail_url, available, price, currency, discount } =
+	const { photo, photo_thumbnail_url, price, currency, discount } =
 		product;
 
 	const handleClickDelete = () => {
@@ -68,9 +65,10 @@ export const BasketItem = ({
 				/>
 				{discountPrice && <Plug className={styles.plug} />}
 			</button>
-			<div className={styles.info}>
+			<div className={styles.info} style={{justifyContent:"justifyContent"}}>
 				<div className={styles.header}>
-					<h4 className={styles.title} onClick={handleClickName}>
+					<h4 className={styles.title} onClick={handleClickName} style={{fontSize:"17px", marginBottom:'7px',
+						width:"90%"}}>
 						{getTranslatedProductName(product)}
 					</h4>{" "}
 					<IconButton
@@ -84,32 +82,22 @@ export const BasketItem = ({
 					title={t("color")}
 					value={getTranslatedColorName(product)}
 				>
-					<ValueBox
-						className={styles.noPointer}
-						value={color.color}
-						color={color.color}
-					/>
 				</ProductVariants>
 				<div className={styles.counterBox}>
-					<ProductVariants
-						className={styles.characteristic}
-						title={t("size")}
-					>
-						<ValueBox
-							className={styles.noPointer}
-							key={size}
-							value={size}
-							title={size}
-							isActive
+					<div style={{display:'flex', flexDirection:'row', fontSize:'15px'}}>
+						<div className={styles.sizeLabel}>{t("size")}:</div>
+						<div> &nbsp; {size}</div>
+					</div>
+					<div style={{ fontSize: '0.75rem' }}>
+						<Counter
+							className={styles.selfTop}
+							value={qty}
+							onChangeCounter={handleChangeCounter}
 						/>
-					</ProductVariants>
-					<Counter
-						className={styles.selfTop}
-						value={qty}
-						onChangeCounter={handleChangeCounter}
-					/>
+					</div>
+
+
 				</div>
-				<AvailableLable isAvailable={available} />
 				<div className={styles.control}>
 					<p>{formatPrice(price, currency)}</p>
 					{discountPrice && (
