05dec164fa760cd34d9338084e838c205e119146
sergeyitaly <sergeyitaly@yahoo.com>
2024-12-16 19:21:54 +0200

    Api middleware

--- logs/middleware.py
+++ logs/middleware.py
@@ -6,38 +6,36 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 class APILogMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        excluded_paths = [ '/admin/']
+        excluded_paths = ['/admin/']
         if any(request.path.startswith(path) for path in excluded_paths):
             return None
-        if request.method == 'DELETE':
-            endpoint = unquote(request.path)
-            remaining_logs = APILog.objects.filter(endpoint=endpoint).count()
-            APILog.objects.filter(endpoint=endpoint).update(request_count=remaining_logs)
-            logger.info(f"Recalculated request count for endpoint {endpoint}: {remaining_logs}")
-            return None 
-
-        endpoint = unquote(request.path)
+        endpoint = unquote(request.path)  # Decode the URL path
+        method = request.method
         has_chat_id = 'by_chat_id' in request.GET or 'by_chat_id' in request.POST
+        if method == 'DELETE':
+            remaining_logs = APILog.objects.filter(endpoint=endpoint).count()
+            logger.info(
+                f"DELETE request received. Current count of logs for endpoint {endpoint}: {remaining_logs}"
+            )
+            return None
         log_entry = APILog.objects.create(
             endpoint=endpoint,
+            method=method,
             has_chat_id=has_chat_id,
-            request_count=1,  
-            timestamp=timezone.localtime(timezone.now())  
+            request_count=1,  # Each log represents one request
+            timestamp=timezone.localtime(timezone.now())  # Save the timestamp
         )
-        self.request_save(endpoint)
+
         logger.info(
-            f"Logged request for endpoint: {endpoint}, has_chat_id: {has_chat_id}, "
-            f"created log entry with ID: {log_entry.id}"
+            f"Logged request: Endpoint={endpoint}, Method={method}, "
+            f"HasChatID={has_chat_id}, LogID={log_entry.id}"
         )
 
     def process_response(self, request, response):
-        logger.debug(f"Response for {request.path} returned with status code {response.status_code}")
+        logger.debug(
+            f"Response for {request.path} returned with status code {response.status_code}"
+        )
         return response
-
-    def request_save(self, endpoint):
-        logs = APILog.objects.filter(endpoint=endpoint)
-        for log in logs:
-            log.save()
-
