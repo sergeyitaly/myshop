736ed914c7838d10e1f914f88240cbfe785f00eb
sergeyitaly <sergeyitaly@yahoo.com>
2024-12-18 23:05:54 +0200

    Team links logging

--- team/middleware.py
+++ team/middleware.py
@@ -1,57 +1,30 @@
 import logging
 from urllib.parse import unquote
+from django.utils.deprecation import MiddlewareMixin
 from django.utils.timezone import now
-from .models import TeamMember
 from logs.models import APILog
+from django.utils import timezone
 
-logger = logging.getLogger('team')
+logger = logging.getLogger(__name__)
 
-
-class TeamMemberLoggingMiddleware:
+class TeamMemberLoggingMiddleware(MiddlewareMixin):
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        self.log_team_member_click(request)
+        endpoint = unquote(request.path)
+        logger.info(f'Request: {endpoint}')
+        self.log_to_database(endpoint)
         response = self.get_response(request)
         return response
 
-    def log_team_member_click(self, request):
-        path = unquote(request.path)
-        team_member, link_type, clicked_link = self.get_team_member_and_link(path)
-
-        if team_member and clicked_link:
-            logger.info(f"Team Member: {team_member}, Link Type: {link_type}, Link: {clicked_link}")
-            self.log_to_database(clicked_link)
-
-    def get_team_member_and_link(self, path):
+    def log_to_database(self, endpoint):
         try:
-            if 'team_member' in path:
-                team_member_id = path.split('/')[-2]  # Adjust this based on your URL structure
-                team_member = TeamMember.objects.get(id=team_member_id)
-                if 'linkedin' in path:
-                    return team_member, 'LinkedIn', team_member.linkedin
-                elif 'github' in path:
-                    return team_member, 'GitHub', team_member.github
-                elif 'behance' in path:
-                    return team_member, 'Behance', team_member.behance
-                elif 'telegram' in path:
-                    return team_member, 'Telegram', team_member.link_to_telegram
-
-        except TeamMember.DoesNotExist:
-            logger.warning(f"No TeamMember found for path: {path}")
-        except Exception as e:
-            logger.error(f"Error while processing path {path}: {e}")
-
-        return None, None, None
-
-    def log_to_database(self, clicked_link):
-        try:
-            APILog.objects.create(
-                endpoint=clicked_link,
+            log_entry = APILog.objects.create(
+                endpoint=endpoint,
                 request_count=1,
-                timestamp=now()
+                timestamp=timezone.localtime(timezone.now())
             )
-            logger.info(f"Successfully logged link: {clicked_link} to the database.")
+            logger.info(f"Logged request: Endpoint={endpoint}, LogID={log_entry.id}, Timestamp={log_entry.timestamp}")
         except Exception as e:
-            logger.error(f"Failed to log clicked link to the database: {e}")
+            logger.error(f"Failed to log request for endpoint: {endpoint}. Error: {e}", exc_info=True)
