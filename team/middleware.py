a365115fa0ded69dd3babb96f42ccecbc368b2b8
sergeyitaly <sergeyitaly@yahoo.com>
2024-12-18 23:17:08 +0200

    Team links middleware logging

--- team/middleware.py
+++ team/middleware.py
@@ -1,30 +1,22 @@
-import logging
-from urllib.parse import unquote
 from django.utils.deprecation import MiddlewareMixin
-from django.utils.timezone import now
-from logs.models import APILog
 from django.utils import timezone
+from .models import APILog
+from urllib.parse import unquote
+import logging
 
 logger = logging.getLogger(__name__)
 
 class TeamMemberLoggingMiddleware(MiddlewareMixin):
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
+    def process_request(self, request):
         endpoint = unquote(request.path)
-        logger.info(f'Request: {endpoint}')
-        self.log_to_database(endpoint)
-        response = self.get_response(request)
-        return response
+        logger.debug(f"Logging request for endpoint: {endpoint}")
+        log_entry = APILog.objects.create(
+            endpoint=endpoint,
+            request_count=1,  # Start with count = 1 for each request
+            timestamp=timezone.localtime(timezone.now())  # Store the exact timestamp
+        )
+        logger.info(f"Logged new request: Endpoint={endpoint}, LogID={log_entry.id}, Timestamp={log_entry.timestamp}")
 
-    def log_to_database(self, endpoint):
-        try:
-            log_entry = APILog.objects.create(
-                endpoint=endpoint,
-                request_count=1,
-                timestamp=timezone.localtime(timezone.now())
-            )
-            logger.info(f"Logged request: Endpoint={endpoint}, LogID={log_entry.id}, Timestamp={log_entry.timestamp}")
-        except Exception as e:
-            logger.error(f"Failed to log request for endpoint: {endpoint}. Error: {e}", exc_info=True)
+    def process_response(self, request, response):
+        logger.debug(f"Response for {request.path} returned with status code {response.status_code}")
+        return response
