d0a3c7feac9fe11a8c8d6d0a5d0f509acb31292f
sergeyitaly <sergeyitaly@yahoo.com>
2024-12-19 01:14:17 +0200

    endpoint domain -

--- logs/middleware.py
+++ logs/middleware.py
@@ -1,35 +1,32 @@
-# middleware.py
 from django.utils.deprecation import MiddlewareMixin
 from django.utils import timezone
 from .models import APILog
-from urllib.parse import unquote
+from urllib.parse import unquote, urlparse
 import logging
+from django.conf import settings
 
 logger = logging.getLogger(__name__)
 
 class APILogMiddleware(MiddlewareMixin):
     def process_request(self, request):
         endpoint = unquote(request.path)
-        
-        host = request.get_host()
-        if host in request.META['HTTP_HOST']: 
-            # If internal, save only the path (no domain)
-            endpoint = endpoint
-        else:
-            # If external, save the full URL
+        if not self.is_internal_request(request):
             endpoint = request.build_absolute_uri()
-        
+        else:
+            endpoint = endpoint
         logger.debug(f"Logging request for endpoint: {endpoint}")
-        
-        # Create a new log entry for the endpoint
         log_entry = APILog.objects.create(
             endpoint=endpoint,
             request_count=1,  # Start with count = 1 for each request
             timestamp=timezone.localtime(timezone.now())  # Store the exact timestamp
         )
-        
+
         logger.info(f"Logged new request: Endpoint={endpoint}, LogID={log_entry.id}, Timestamp={log_entry.timestamp}")
 
     def process_response(self, request, response):
         logger.debug(f"Response for {request.path} returned with status code {response.status_code}")
         return response
+
+    def is_internal_request(self, request):
+        host = request.get_host()
+        return host == "localhost:8000" or "127.0.0.1:8000" or host.endswith(settings.VERCEL_DOMAIN)
