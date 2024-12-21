305f30d191d26f072b2b76904c9cc5af5c8f1bfc
sergeyitaly <sergeyitaly@yahoo.com>
2024-12-22 01:55:20 +0200

    API log middlware fix

--- logs/middleware.py
+++ logs/middleware.py
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 
 class APILogMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        current_timestamp = timezone.localtime(timezone.now()).replace(microsecond=0)
+        current_timestamp = timezone.localtime(timezone.now()).replace(second=0, microsecond=0)
         is_android = self.is_android_request(request)
         is_vercel = self.is_vercel_request(request)
         endpoint = unquote(request.build_absolute_uri())
@@ -20,12 +20,25 @@ class APILogMiddleware(MiddlewareMixin):
 
         if is_vercel:
             logger.debug(f"Request processed by Vercel detected for {endpoint}")
-        log_entry = APILog.objects.create(
+
+        platform = "Android" if is_android else "Vercel" if is_vercel else "Other"
+
+        # Check for an existing log entry
+        if not APILog.objects.filter(
             endpoint=endpoint,
-            request_count=1,  # Always set to 1
+            platform=platform,
             timestamp=current_timestamp
-        )
-        logger.info(f"Logged request: Endpoint={endpoint}, LogID={log_entry.id}, Timestamp={log_entry.timestamp}")
+        ).exists():
+            # Create a new log entry only if it doesn't already exist
+            log_entry = APILog.objects.create(
+                endpoint=endpoint,
+                platform=platform,
+                request_count=1,  # Always set to 1
+                timestamp=current_timestamp
+            )
+            logger.info(f"Logged request: Endpoint={endpoint}, LogID={log_entry.id}, Timestamp={log_entry.timestamp}")
+        else:
+            logger.debug(f"Duplicate log skipped for Endpoint={endpoint}, Platform={platform}, Timestamp={current_timestamp}")
 
     def process_response(self, request, response):
         logger.debug(f"Response for {request.path} returned with status code {response.status_code}")
