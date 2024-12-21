e8e7f7b4bac85cb08b4ff91b05ab4e0470aec827
sergeyitaly <sergeyitaly@yahoo.com>
2024-12-22 01:08:43 +0200

    API log middlware fix

--- logs/middleware.py
+++ logs/middleware.py
@@ -13,12 +13,15 @@ class APILogMiddleware(MiddlewareMixin):
         is_vercel = self.is_vercel_request(request)
         endpoint = unquote(request.build_absolute_uri())
         endpoint = endpoint.replace('http://', '', 1)
-        if is_android:
-            logger.debug(f"Android WebView request detected for {endpoint}")
-        if is_vercel and self.is_android_origin(request):
-            endpoint = endpoint.replace('https://', '', 1)
-            logger.debug(f"Skipping Vercel request for endpoint {endpoint} since it's caused by Android WebView.")
-            return
+
+        if is_android and is_vercel:
+            logger.debug(f"Android request detected for {endpoint}")
+            if endpoint.startswith('https://'):
+                endpoint = endpoint.replace('https://', '', 1)
+
+        if is_vercel:
+            logger.debug(f"Request processed by Vercel detected for {endpoint}")
+
         time_window_start = current_timestamp - timezone.timedelta(seconds=2)
         existing_log = APILog.objects.filter(
             endpoint=endpoint,
@@ -41,7 +44,6 @@ class APILogMiddleware(MiddlewareMixin):
         return response
 
     def is_android_request(self, request):
-        # Check if the request has the Android-specific header or User-Agent
         if request.headers.get('X-Android-Client') == 'Koloryt':
             return True
         user_agent = request.headers.get('User-Agent', '').lower()
@@ -50,11 +52,6 @@ class APILogMiddleware(MiddlewareMixin):
         return False
 
     def is_vercel_request(self, request):
-        # Identify requests served by Vercel
         if request.headers.get('X-Vercel-Client'):
             return True
         return request.META.get('SERVER_NAME', '').endswith('.vercel.app')
-
-    def is_android_origin(self, request):
-        # Check if the request originates from Android WebView (via headers or meta data)
-        return request.headers.get('X-Android-Origin') == 'True'
