0eb230fb85412d73e2c9b2fe9148322b9e5ee688
sergeyitaly <sergeyitaly@yahoo.com>
2024-12-24 21:15:51 +0200

    middlware api

--- logs/middleware.py
+++ logs/middleware.py
@@ -10,17 +10,13 @@ class APILogMiddleware(MiddlewareMixin):
     def process_request(self, request):
         current_timestamp = timezone.localtime(timezone.now()).replace(second=0, microsecond=0)
         endpoint = unquote(request.build_absolute_uri()).replace('http://', '')
-        if self.is_vercel_request(request) and self.is_android_webview_request(request):
-            logger.debug(f"Vercel request caused by Android WebView detected for {endpoint}. Dropping the request.")
-            return None  # Drop the request if it's a Vercel request caused by Android WebView
-        if self.is_vercel_request(request):
+        if self.is_vercel_request(request) or self.is_android_request(request):
             logger.debug(f"Regular Vercel request detected for {endpoint}")
             some_seconds_ago = timezone.now() - timezone.timedelta(seconds=10)
-            duplicate = APILog.objects.filter(endpoint=endpoint, timestamp__gte=some_seconds_ago).first()
-
+            duplicate = APILog.objects.filter(endpoint=endpoint.replace('https://', ''), timestamp__gte=some_seconds_ago).first()
             if duplicate:
                 logger.debug(f"Duplicate request detected for {endpoint}. Skipping log.")
-                return None  # Skip logging duplicate requests
+                return None
             else:
                 log_entry = APILog.objects.create(
                     endpoint=endpoint,
@@ -28,7 +24,14 @@ class APILogMiddleware(MiddlewareMixin):
                     timestamp=current_timestamp
                 )
                 logger.info(f"Logged Vercel request: Endpoint={endpoint}, LogID={log_entry.id}, Timestamp={log_entry.timestamp}")
-                return None  # Allow the request to proceed after logging
+                return None
+        else:
+            log_entry = APILog.objects.create(
+                    endpoint=endpoint,
+                    request_count=1,
+                    timestamp=current_timestamp
+                )
+            logger.info(f"Logged Vercel request: Endpoint={endpoint}, LogID={log_entry.id}, Timestamp={log_entry.timestamp}")
 
     def process_response(self, request, response):
         logger.debug(f"Response for {request.path} returned with status code {response.status_code}")
@@ -36,16 +39,13 @@ class APILogMiddleware(MiddlewareMixin):
 
     def is_android_request(self, request):
         if request.headers.get('X-Android-Client') == 'Koloryt':
-            return True  # This is a known Android client request
+            return True
         user_agent = request.headers.get('User-Agent', '').lower()
         if "android" in user_agent and "webview" in user_agent:
-            return True  # It's an Android WebView request
+            return True
         return False
 
     def is_vercel_request(self, request):
         if request.headers.get('X-Vercel-Client'):
             return True
         return request.META.get('SERVER_NAME', '').endswith('.vercel.app')
-
-    def is_android_webview_request(self, request):
-        return self.is_android_request(request) and self.is_vercel_request(request)
