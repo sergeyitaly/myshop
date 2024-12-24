9a0bdc5352e34328cd0b2a082aef72fd357f9c6d
sergeyitaly <sergeyitaly@yahoo.com>
2024-12-24 21:25:59 +0200

    middlware api update

--- logs/middleware.py
+++ logs/middleware.py
@@ -10,7 +10,7 @@ class APILogMiddleware(MiddlewareMixin):
     def process_request(self, request):
         current_timestamp = timezone.localtime(timezone.now()).replace(second=0, microsecond=0)
         endpoint = unquote(request.build_absolute_uri()).replace('http://', '')
-        if self.is_vercel_request(request) or self.is_android_request(request):
+        if self.is_android_request(request):
             logger.debug(f"Regular Vercel request detected for {endpoint}")
             some_seconds_ago = timezone.now() - timezone.timedelta(seconds=10)
             duplicate = APILog.objects.filter(endpoint=endpoint.replace('https://', ''), timestamp__gte=some_seconds_ago).first()
@@ -40,12 +40,3 @@ class APILogMiddleware(MiddlewareMixin):
     def is_android_request(self, request):
         if request.headers.get('X-Android-Client') == 'Koloryt':
             return True
-        user_agent = request.headers.get('User-Agent', '').lower()
-        if "android" in user_agent and "webview" in user_agent:
-            return True
-        return False
-
-    def is_vercel_request(self, request):
-        if request.headers.get('X-Vercel-Client'):
-            return True
-        return request.META.get('SERVER_NAME', '').endswith('.vercel.app')
