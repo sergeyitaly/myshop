ce3efa483c5aeb46afd46398111d62378cdd96fe
olyflower <2013.olga.yudina@gmail.com>
2024-12-26 17:08:35 +0200

    add payment, delivery in order

--- frontend/src/components/Forms/OrderForm/OrderForm.tsx
+++ frontend/src/components/Forms/OrderForm/OrderForm.tsx
@@ -4,15 +4,15 @@ import clsx from "clsx";
 import { FormikInput } from "../../UI/FormikInput/FormikInput";
 import { Form, Formik } from "formik";
 import { FormikCheckBox } from "../../UI/FormikCheckBox/FormikCheckBox";
+import { FormikRadioButton } from "../../UI/FormikRadioButton/FormikRadioButton";
 import { FormikTextArea } from "../../UI/FormikTextArea/FormikTextArea";
 import { OrderFormBox } from "../../../pages/Order/OrderFormBox/OrderFormBox";
 import { MainButton } from "../../UI/MainButton/MainButton";
-import { OrderInfo } from "../../OrderInfo/OrderInfo";
 import { CheckBoxComment } from "../../CheckBoxComment/CheckBoxComment";
 import { OrderDTO } from "../../../models/dto";
 import {
-    CreateOrderErrorResponce,
-    useCreateOrderMutation,
+	CreateOrderErrorResponce,
+	useCreateOrderMutation,
 } from "../../../api/orderSlice";
 import { getOrderValidSchema } from "./validationSchema";
 import { ServerErrorHandling } from "./ServerErrorHandling";
@@ -21,169 +21,205 @@ import { useBasket } from "../../../hooks/useBasket";
 import { EmptyItemsErrorHandler } from "../EmptyItemsErrorHandler";
 import { ProductAndPriceFormikUpdate } from "./ProductAndPriceFormikUpdate/ProductAndPriceFormikUpdate";
 import { useTranslation } from "react-i18next";
+import { STORAGE } from "../../../constants";
 import bird from "./bird.svg";
 import styles from "./OrderForm.module.scss";
-import { STORAGE } from "../../../constants";
 
 interface OrderFormProps {
-    className?: string;
+	className?: string;
 }
 
 export const OrderForm = ({ className }: OrderFormProps) => {
-    const { t } = useTranslation();
-    const navigate = useNavigate();
-    const [isOpenComment, setOpenComment] = useState<boolean>(false);
-    const [isOpenCongrats, setOpenCongrats] = useState<boolean>(false);
-    const { clearBasket } = useBasket();
-    const [createOrder, { isSuccess, error, isError, isLoading }] =
-        useCreateOrderMutation();
+	const { t } = useTranslation();
+	const navigate = useNavigate();
+	const [isOpenComment, setOpenComment] = useState<boolean>(false);
+	const [isOpenCongrats, setOpenCongrats] = useState<boolean>(false);
+	const { clearBasket } = useBasket();
+	const [createOrder, { isSuccess, error, isError, isLoading }] =
+		useCreateOrderMutation();
 
-    const [language, setLanguage] = useState<string>(
-        localStorage.getItem(STORAGE.LANGUAGE) || "uk"
-    );
+	const [language, setLanguage] = useState<string>(
+		localStorage.getItem(STORAGE.LANGUAGE) || "uk"
+	);
 
-    useEffect(() => {
-        // Update language in localStorage when it changes
-        if (language) {
-            localStorage.setItem(STORAGE.LANGUAGE, language);
-        }
-    }, [language]);
+	useEffect(() => {
+		if (language) {
+			localStorage.setItem(STORAGE.LANGUAGE, language);
+		}
+	}, [language]);
 
-    useEffect(() => {
-        const storedLanguage = localStorage.getItem(STORAGE.LANGUAGE) || "uk";
-        setLanguage(storedLanguage);
-    }, []);
+	useEffect(() => {
+		const storedLanguage = localStorage.getItem(STORAGE.LANGUAGE) || "uk";
+		setLanguage(storedLanguage);
+	}, []);
 
-    const initialValues: OrderDTO = {
-        name: "",
-        surname: "",
-        email: "",
-        phone: "+380",
-        receiver: false,
-        present: false,
-        order_items: [],
-        receiver_comments: "",
-        congrats: "",
-        language: language,
-    };
+	const initialValues: OrderDTO = {
+		name: "",
+		surname: "",
+		email: "",
+		phone: "+380",
+		receiver: false,
+		present: false,
+		order_items: [],
+		receiver_comments: "",
+		congrats: "",
+		language: language,
+		delivery: "self_pickup",
+		payment: "cash_on_delivery",
+	};
 
-    let errorResponce: CreateOrderErrorResponce | null = null;
-    if (isError) {
-        errorResponce = error as CreateOrderErrorResponce;
-    }
+	let errorResponce: CreateOrderErrorResponce | null = null;
+	if (isError) {
+		errorResponce = error as CreateOrderErrorResponce;
+	}
 
-    useEffect(() => {
-        if (isSuccess) {
-            navigate(ROUTE.THANK);
-            clearBasket();
-        }
-    }, [isSuccess]);
+	useEffect(() => {
+		if (isSuccess) {
+			navigate(ROUTE.THANK);
+			clearBasket();
+		}
+	}, [isSuccess]);
 
-    const handleClickAddComment = () => {
-        setOpenComment(!isOpenComment);
-    };
+	const handleClickAddComment = () => {
+		setOpenComment(!isOpenComment);
+	};
 
-    const handleClickAddCongrats = () => {
-        setOpenCongrats(!isOpenCongrats);
-    };
+	const handleClickAddCongrats = () => {
+		setOpenCongrats(!isOpenCongrats);
+	};
 
-    return (
-        <Formik
-            initialValues={{
-                ...initialValues,
-            }}
-            validationSchema={getOrderValidSchema(t)}
-            onSubmit={(form) => {
-                createOrder(form);
-            }}
-        >
-            <Form className={clsx(styles.form, className)}>
-                <OrderFormBox title={t("recipient")}>
-                    <div className={styles.inputsContainer}>
-                        <FormikInput label={t("first_name")} name="name" />
-                        <FormikInput label={t("last_name")} name="surname" />
-                        <FormikInput label={t("phone")} name="phone" />
-                        <FormikInput label={t("email")} name="email" />
+	return (
+		<Formik
+			initialValues={{
+				...initialValues,
+			}}
+			validationSchema={getOrderValidSchema(t)}
+			onSubmit={(form) => {
+				createOrder(form);
+			}}
+		>
+			<Form className={clsx(styles.form, className)}>
+				<OrderFormBox title={t("recipient")}>
+					<div className={styles.inputsContainer}>
+						<FormikInput label={t("first_name")} name="name" />
+						<FormikInput label={t("last_name")} name="surname" />
+						<FormikInput label={t("phone")} name="phone" />
+						<FormikInput label={t("email")} name="email" />
 
-                        <div className={clsx(styles.anotherPersonBox)}>
-                            <FormikCheckBox name="receiver" />
-                            <CheckBoxComment
-                                text={t("receiver_checkbox_label")}
-                            />
-                            <button
-                                className={styles.commentButton}
-                                type="button"
-                                onClick={handleClickAddComment}
-                            >
-                                {!isOpenComment
-                                    ? t("add_comment")
-                                    : t("remove_comment")}
-                            </button>
-                            {isOpenComment && (
-                                <FormikTextArea
-                                    className={styles.textArea}
-                                    name="receiver_comments"
-                                    placeholder={t("comment_placeholder")}
-                                />
-                            )}
-                            <EmptyItemsErrorHandler name="order_items" />
-                        </div>
-                    </div>
-                </OrderFormBox>
+						<div className={clsx(styles.anotherPersonBox)}>
+							<FormikCheckBox name="receiver" />
+							<CheckBoxComment
+								text={t("receiver_checkbox_label")}
+							/>
+							<button
+								className={styles.commentButton}
+								type="button"
+								onClick={handleClickAddComment}
+							>
+								{!isOpenComment
+									? t("add_comment")
+									: t("remove_comment")}
+							</button>
+							{isOpenComment && (
+								<FormikTextArea
+									className={styles.textArea}
+									name="receiver_comments"
+									placeholder={t("comment_placeholder")}
+								/>
+							)}
+							<EmptyItemsErrorHandler name="order_items" />
+						</div>
+					</div>
+				</OrderFormBox>
 
-                <OrderFormBox title={t("pickup_store")}>
+				{/* <OrderFormBox title={t("pickup_store")}>
                     <OrderInfo text={t("pickup_store")} />
                 </OrderFormBox>
 
                 <OrderFormBox title={t("payment_on_delivery")}>
                     <OrderInfo text={t("payment_on_delivery")} />
-                </OrderFormBox>
+                </OrderFormBox> */}
 
-                <OrderFormBox title={t("additionaly")}>
-                    <div className={styles.giftWrapper}>
-                        <div className={styles.giftCheckbox}>
-                            <FormikCheckBox name="present" />
-                            <CheckBoxComment text={t("wrap_as_gift")} />
-                        </div>
+				<OrderFormBox title={t("delivery.label")}>
+					<div className={styles.radioGroup}>
+						<FormikRadioButton
+							name="delivery"
+							value="self_pickup"
+							label={t("delivery.self_pickup")}
+						/>
+						<FormikRadioButton
+							name="delivery"
+							value="nova_poshta"
+							label={t("delivery.nova_poshta")}
+						/>
+						<FormikRadioButton
+							name="delivery"
+							value="ukr_poshta"
+							label={t("delivery.ukr_poshta")}
+						/>
+					</div>
+				</OrderFormBox>
 
-                        <div className={styles.giftContainer}>
-                            <p className={styles.giftTitle}>
-                                <img src={bird} alt={t("gift_title")} />{" "}
-                                {t("gift_title")}
-                            </p>
-                            <button
-                                type="button"
-                                className={styles.giftButton}
-                                onClick={handleClickAddCongrats}
-                            >
-                                <span>
-                                    {!isOpenCongrats
-                                        ? t("add_congrats")
-                                        : t("remove_congrats")}
-                                </span>
-                            </button>
+				<OrderFormBox title={t("payment.label")}>
+					<div className={styles.radioGroup}>
+						<FormikRadioButton
+							name="payment"
+							value="cash_on_delivery"
+							label={t("payment.cash_on_delivery")}
+						/>
+						<FormikRadioButton
+							name="payment"
+							value="card_online"
+							label={t("payment.card_online")}
+						/>
+					</div>
+				</OrderFormBox>
 
-                            {isOpenCongrats && (
-                                <FormikTextArea
-                                    className={styles.giftTextArea}
-                                    name="congrats"
-                                    placeholder={t("gift_message")}
-                                />
-                            )}
-                        </div>
-                    </div>
-                </OrderFormBox>
+				<OrderFormBox title={t("additionaly")}>
+					<div className={styles.giftWrapper}>
+						<div className={styles.giftCheckbox}>
+							<FormikCheckBox name="present" />
+							<CheckBoxComment text={t("wrap_as_gift")} />
+						</div>
 
-                <MainButton
-                    title={isLoading ? t("loading") : t("place_order")}
-                    color="black"
-                    disabled={isLoading}
-                />
-                <ServerErrorHandling
-                    errors={errorResponce && errorResponce.data}
-                />
-                <ProductAndPriceFormikUpdate />
-            </Form>
-        </Formik>
-    );
+						<div className={styles.giftContainer}>
+							<p className={styles.giftTitle}>
+								<img src={bird} alt={t("gift_title")} />{" "}
+								{t("gift_title")}
+							</p>
+							<button
+								type="button"
+								className={styles.giftButton}
+								onClick={handleClickAddCongrats}
+							>
+								<span>
+									{!isOpenCongrats
+										? t("add_congrats")
+										: t("remove_congrats")}
+								</span>
+							</button>
+
+							{isOpenCongrats && (
+								<FormikTextArea
+									className={styles.giftTextArea}
+									name="congrats"
+									placeholder={t("gift_message")}
+								/>
+							)}
+						</div>
+					</div>
+				</OrderFormBox>
+
+				<MainButton
+					title={isLoading ? t("loading") : t("place_order")}
+					color="black"
+					disabled={isLoading}
+				/>
+				<ServerErrorHandling
+					errors={errorResponce && errorResponce.data}
+				/>
+				<ProductAndPriceFormikUpdate />
+			</Form>
+		</Formik>
+	);
 };
