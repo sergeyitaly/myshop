bc26445d17838f65817c055fde508ac6a9e34d62
sergeyitaly <sergeyitaly@yahoo.com>
2024-12-26 22:28:44 +0200

    api middlware log

--- logs/middleware.py
+++ logs/middleware.py
@@ -13,19 +13,12 @@ class APILogMiddleware(MiddlewareMixin):
         endpoint = endpoint.replace("http://", "")
 
         some_seconds_ago = current_timestamp - timezone.timedelta(seconds=10)
-        # Check if it's an Android WebView request (no https:// part)
         is_android_webview = (
-            "https://" not in endpoint and request.headers.get('X-Android-Client', '').lower() == 'koloryt'
+            request.headers.get('X-Android-Client', '').lower() == 'koloryt'
         )
-        
-        # Check if it's a Vercel request (has https:// part)
         is_vercel_request = (
-            "https://" in endpoint
-            and request.META.get('SERVER_NAME', '').endswith('.vercel.app')
-            and request.is_secure()
-        )
-        
-        # Check if it's a local HTTP request
+            request.META.get('SERVER_NAME', '').endswith('.vercel.app')
+            )
         is_local_request = "http://" in endpoint
 
         logger.debug(
@@ -35,9 +28,7 @@ class APILogMiddleware(MiddlewareMixin):
             f"is_local_request={is_local_request}"
         )
         logger.debug(f"Headers: {request.headers}")
-
         if is_android_webview or is_vercel_request or is_local_request:
-            # Normalize endpoint and log the request
             self.log_request(
                 endpoint,
                 current_timestamp=current_timestamp,
@@ -53,17 +44,9 @@ class APILogMiddleware(MiddlewareMixin):
         return response
 
     def log_request(self, endpoint, current_timestamp, some_seconds_ago, is_android_webview, is_vercel_request):
-        if is_android_webview:
-            endpoint_normalized = endpoint.replace("https://", "").strip() if "https://" in endpoint else endpoint
-        elif is_vercel_request:
-            endpoint_normalized = endpoint.replace("https://", "").strip()
-        else:
-            endpoint_normalized = endpoint.replace("http://", "")
-
-
-        duplicates = APILog.objects.filter(endpoint=endpoint_normalized, timestamp__gte=some_seconds_ago)
-        if duplicates.exists():
-            logger.info(f"Deleting {duplicates.count()} duplicate logs for endpoint={endpoint_normalized}")
+        duplicates = APILog.objects.filter(endpoint=endpoint.replace("https://", "").strip(), timestamp__gte=some_seconds_ago)
+        if duplicates.first():
+            logger.info(f"Deleting {duplicates.count()} duplicate logs for endpoint={endpoint}")
             duplicates.delete()
 
         log_entry = APILog.objects.create(
@@ -71,9 +54,7 @@ class APILogMiddleware(MiddlewareMixin):
             request_count=1,
             timestamp=current_timestamp,
         )
-
         logger.info(
-            f"Logged endpoint={endpoint_normalized}, LogID={log_entry.id}, "
             f"Timestamp={log_entry.timestamp}, "
             f"RequestType={'AndroidWebView' if is_android_webview else 'VercelRequest' if is_vercel_request else 'Unknown'}"
         )
