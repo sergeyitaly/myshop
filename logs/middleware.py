80415ae8062ce0b272d94040d07d5791f719b211
sergeyitaly <sergeyitaly@yahoo.com>
2024-12-26 10:06:20 +0200

    logs middlware

--- logs/middleware.py
+++ logs/middleware.py
@@ -6,44 +6,34 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-
 class APILogMiddleware(MiddlewareMixin):
     def process_request(self, request):
         current_timestamp = timezone.localtime(timezone.now()).replace(second=0, microsecond=0)
-        endpoint = unquote(request.build_absolute_uri()).replace('http://','')
-        some_seconds_ago = timezone.now() - timezone.timedelta(seconds=10)
-
-        if self.is_android_webview_request(request, endpoint):
-            self.log_request(endpoint, is_webview=True, current_timestamp=current_timestamp, some_seconds_ago=some_seconds_ago)
-        elif self.is_vercel_production_request(request, endpoint):
-            self.log_request(endpoint, is_webview=False, current_timestamp=current_timestamp, some_seconds_ago=some_seconds_ago)
-        else:
-            self.log_request(endpoint, is_webview=False, current_timestamp=current_timestamp, some_seconds_ago=some_seconds_ago)
-        
+        endpoint = unquote(request.build_absolute_uri()).replace('http://', '').replace('https://', '').strip()
+        some_seconds_ago = current_timestamp - timezone.timedelta(seconds=10)
+        if self.should_log_request(request, endpoint):
+            self.log_request(endpoint, current_timestamp=current_timestamp, some_seconds_ago=some_seconds_ago)
         return None
 
     def process_response(self, request, response):
         logger.debug(f"Response for {request.path} returned with status code {response.status_code}")
         return response
 
-    def is_android_webview_request(self, request, endpoint):
-        return "https://" not in endpoint and request.headers.get('X-Android-Client') == 'Koloryt'
-
-    def is_vercel_production_request(self, request, endpoint):
-        return (
-            "https://" in endpoint 
-            and request.META.get('SERVER_NAME', '').endswith('.vercel.app') 
+    def should_log_request(self, request, endpoint):
+        is_android_webview = (
+            "https://" not in endpoint
+            and request.headers.get('X-Android-Client') == 'Koloryt'
+        )
+        is_vercel_request = (
+            "https://" in endpoint
+            and request.META.get('SERVER_NAME', '').endswith('.vercel.app')
             and request.is_secure()
         )
+        return is_android_webview or is_vercel_request
 
-    def log_request(self, endpoint, is_webview, current_timestamp, some_seconds_ago):
-        if is_webview:
-            endpoint = endpoint.replace('https://', '').strip()
-        else:
-            endpoint = endpoint.strip()
-        duplicate = APILog.objects.filter(endpoint=endpoint, timestamp__gte=some_seconds_ago)
-
-        if not duplicate.exists():
+    def log_request(self, endpoint, current_timestamp, some_seconds_ago):
+        duplicate = APILog.objects.filter(endpoint=endpoint, timestamp__gte=some_seconds_ago).exists()
+        if not duplicate:
             log_entry = APILog.objects.create(
                 endpoint=endpoint,
                 request_count=1,
