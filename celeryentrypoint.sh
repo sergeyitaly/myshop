987aa6a0133fbd50c25b4cd3554454e7b939956f
sergeyitaly <sergeyitaly@yahoo.com>
2025-01-06 01:58:59 +0200

    celery concurency 1

--- celeryentrypoint.sh
+++ celeryentrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Start Celery worker with memory limit and reduced concurrency
-celery -A myshop worker --loglevel=info --max-memory-per-child=600000 --concurrency=2 &
+celery -A myshop worker --loglevel=info --max-memory-per-child=200000 --concurrency=1 &
 
-# Start Celery Beat for periodic tasks with a reduced number of workers
-celery -A myshop beat --loglevel=info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
+# Start Celery Beat for periodic tasks with memory limit
+celery -A myshop beat --loglevel=info --max-memory-per-child=200000 --scheduler django_celery_beat.schedulers:DatabaseScheduler &
 
 # Start Django application with gevent workers (asynchronous) to reduce memory usage
-gunicorn myshop.wsgi:application --bind 0.0.0.0:8000 --workers 2 --worker-class gevent --timeout 120
+gunicorn myshop.wsgi:application --bind 0.0.0.0:8000 --workers 1 --worker-class gevent --timeout 120
