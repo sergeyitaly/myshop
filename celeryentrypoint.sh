ce2c10ab1c43da33946a66b3613d936bebc69587
sergeyitaly <sergeyitaly@yahoo.com>
2025-01-06 01:43:46 +0200

    gevent for celery

--- celeryentrypoint.sh
+++ celeryentrypoint.sh
@@ -6,5 +6,5 @@ celery -A myshop worker --loglevel=info --max-memory-per-child=600000 --concurre
 # Start Celery Beat for periodic tasks with a reduced number of workers
 celery -A myshop beat --loglevel=info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
 
-# Start Django application with a reduced number of workers
-gunicorn myshop.wsgi:application --bind 0.0.0.0:8000 --workers 2 --worker-class sync --timeout 120
+# Start Django application with gevent workers (asynchronous) to reduce memory usage
+gunicorn myshop.wsgi:application --bind 0.0.0.0:8000 --workers 2 --worker-class gevent --timeout 120
