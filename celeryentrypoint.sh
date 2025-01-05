dd3fc191c89898bbc39a9e4efae4ade386969b31
sergeyitaly <sergeyitaly@yahoo.com>
2025-01-06 01:28:03 +0200

    fly toml

--- celeryentrypoint.sh
+++ celeryentrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Start Celery worker
-celery -A myshop worker --loglevel=info &
+# Start Celery worker with memory limit and reduced concurrency
+celery -A myshop worker --loglevel=info --max-memory-per-child=600000 --concurrency=2 &
 
-# Start Celery Beat for periodic tasks
+# Start Celery Beat for periodic tasks with a reduced number of workers
 celery -A myshop beat --loglevel=info --scheduler django_celery_beat.schedulers:DatabaseScheduler &
 
-# Start Django application
-gunicorn myshop.wsgi:application --bind 0.0.0.0:8000
+# Start Django application with a reduced number of workers
+gunicorn myshop.wsgi:application --bind 0.0.0.0:8000 --workers 2 --worker-class sync --timeout 120
