99b57d22605f7d2e9dd4815fc276e2cebbd3586a
sergeyitaly <sergeyitaly@yahoo.com>
2025-01-06 19:06:04 +0200

    order utils atomic

--- order/utils.py
+++ order/utils.py
@@ -1,16 +1,8 @@
 from django.utils import timezone
-from order.models import Order, OrderSummary
-from .notifications import update_order_status_with_notification
-from django.utils.dateformat import format as date_format
-from django.db import transaction
-from django.utils.translation import gettext as _  # Import gettext for translation
-import requests
-from django.conf import settings
-from .models import *
-from celery import shared_task
-from django.utils import timezone
-from datetime import timedelta
 from order.models import Order
+from datetime import timedelta
+from celery import shared_task
+from django.db import transaction
 
 @shared_task
 def update_order_statuses():
@@ -18,14 +10,15 @@ def update_order_statuses():
 
     def update_orders(current_status, new_status, minutes, status_field):
         threshold_time = now - timedelta(minutes=minutes)
-        orders_to_update = Order.objects.filter(
-            status=current_status,
-            **{f"{status_field}__lte": threshold_time}
-        )
-        updated_count = orders_to_update.update(
-            status=new_status,
-            **{f"{new_status}_at": now}
-        )
+        with transaction.atomic():  
+            orders_to_update = Order.objects.filter(
+                status=current_status,
+                **{f"{status_field}__lte": threshold_time}
+            )
+            updated_count = orders_to_update.update(
+                status=new_status,
+                **{f"{new_status}_at": now}
+            )
         return updated_count
 
     submitted_to_created = update_orders(
@@ -34,16 +27,18 @@ def update_order_statuses():
         minutes=1,
         status_field='submitted_at'
     )
+
     created_to_processed = update_orders(
         current_status='created',
         new_status='processed',
         minutes=20,
         status_field='created_at'
     )
+
     processed_to_complete = update_orders(
         current_status='processed',
         new_status='complete',
-        minutes=24 * 60,  
+        minutes=24 * 60,
         status_field='processed_at'
     )
 
@@ -52,35 +47,3 @@ def update_order_statuses():
         'created_to_processed': created_to_processed,
         'processed_to_complete': processed_to_complete,
     }
-
-
-def datetime_to_str(dt):
-    return date_format(dt, 'Y-m-d H:i') if dt else None
-
-def send_mass_message_with_logging(telegram_message):
-    bot_token = settings.NOTIFICATIONS_API
-    base_url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
-    website_url = settings.VERCEL_DOMAIN
-
-    users = TelegramUser.objects.all()
-    sent_users = []
-
-    for user in users:
-        message_with_link = (
-            f"{telegram_message.content}\n\n"
-            f"<a href='{website_url}'>Visit our website</a>"
-        )
-        payload = {
-            'chat_id': user.chat_id,
-            'text': message_with_link,
-            'parse_mode': 'HTML',
-        }
-
-        try:
-            response = requests.post(base_url, json=payload)
-            response.raise_for_status()
-            sent_users.append(user)
-        except requests.exceptions.RequestException as e:
-            print(f"Failed to send message to {user.chat_id}: {e}")
-
-    telegram_message.sent_to.add(*sent_users) 
