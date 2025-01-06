17fd01e57a533e0054404e87d7182637109f51ca
sergeyitaly <sergeyitaly@yahoo.com>
2025-01-06 17:50:31 +0200

    celery py fix

--- myshop/celery.py
+++ myshop/celery.py
@@ -5,11 +5,10 @@ from django.conf import settings
 from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshop.settings')
-
 app = Celery('myshop')
-
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
+# Celery-specific configurations
 app.conf.broker_connection_retry_on_startup = True
 app.conf.task_serializer = 'json'
 app.conf.result_serializer = 'json'
@@ -19,26 +18,26 @@ app.conf.timezone = 'UTC'
 # Beat schedule configuration
 app.conf.beat_schedule = {
     'update-order-statuses-every-minute': {
-        'task': 'order.tasks.update_order_statuses',
+        'task': 'order.tasks.update_order_statuses_task',
         'schedule': crontab(minute='*/1'), 
     },
 }
 
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 app.conf.update(
-    BROKER_URL=settings.CELERY_BROKER_URL,
-    CELERY_BEAT_SCHEDULE_FILENAME='celerybeat-schedule',
-    CELERY_RESULT_BACKEND=settings.CELERY_RESULT_BACKEND,
-    CELERY_IMPORTS=('order.tasks',),
-    CELERY_WORKER_POOL_RESTARTS=True,
-    CELERY_WORKER_MAX_TASKS_PER_CHILD=1000,
-    CELERY_WORKER_PREFETCH_MULTIPLIER=1,
-    CELERY_WORKER_CANCEL_LONG_RUNNING_TASKS_ON_CONNECTION_LOSS=True,
+    BROKER_URL=settings.BROKER_URL, 
+    RESULT_BACKEND=settings.RESULT_BACKEND,  
+    BEAT_SCHEDULE_FILENAME='celerybeat-schedule', 
+    IMPORTS=('order.tasks',),
+    WORKER_POOL_RESTARTS=True,
+    WORKER_MAX_TASKS_PER_CHILD=1000,
+    WORKER_PREFETCH_MULTIPLIER=1,
+    WORKER_CANCEL_LONG_RUNNING_TASKS_ON_CONNECTION_LOSS=True,
     BROKER_POOL_LIMIT=None,
     BROKER_TRANSPORT_OPTIONS={
         'fanout_prefix': True,
         'fanout_patterns': True,
         'max_connections': 50,
         'socket_keepalive': True,
-    }
+    },
 )
