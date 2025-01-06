5b67b01291ddd752b4a209ada145eb17d83452df
sergeyitaly <sergeyitaly@yahoo.com>
2025-01-06 19:12:34 +0200

    order utils fix

--- order/utils.py
+++ order/utils.py
@@ -1,4 +1,18 @@
 from django.utils import timezone
+from order.models import Order, OrderSummary
+from .notifications import update_order_status_with_notification
+from django.utils.dateformat import format as date_format
+from django.db import transaction
+from django.utils.translation import gettext as _  # Import gettext for translation
+import requests
+from django.conf import settings
+from .models import *
+from celery import shared_task
+from django.utils import timezone
+from datetime import timedelta
+from order.models import Order
+
+from django.utils import timezone
 from order.models import Order
 from datetime import timedelta
 from celery import shared_task
@@ -47,3 +61,34 @@ def update_order_statuses():
         'created_to_processed': created_to_processed,
         'processed_to_complete': processed_to_complete,
     }
+
+def datetime_to_str(dt):
+    return date_format(dt, 'Y-m-d H:i') if dt else None
+
+def send_mass_message_with_logging(telegram_message):
+    bot_token = settings.NOTIFICATIONS_API
+    base_url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+    website_url = settings.VERCEL_DOMAIN
+
+    users = TelegramUser.objects.all()
+    sent_users = []
+
+    for user in users:
+        message_with_link = (
+            f"{telegram_message.content}\n\n"
+            f"<a href='{website_url}'>Visit our website</a>"
+        )
+        payload = {
+            'chat_id': user.chat_id,
+            'text': message_with_link,
+            'parse_mode': 'HTML',
+        }
+
+        try:
+            response = requests.post(base_url, json=payload)
+            response.raise_for_status()
+            sent_users.append(user)
+        except requests.exceptions.RequestException as e:
+            print(f"Failed to send message to {user.chat_id}: {e}")
+
+    telegram_message.sent_to.add(*sent_users) 
