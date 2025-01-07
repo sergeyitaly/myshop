24706f60b3cafdca0c16d3bafbd85d90c7486ed0
sergeyitaly <sergeyitaly@yahoo.com>
2025-01-07 10:41:41 +0200

    dockerfile and toml

--- start_celery.sh
+++ start_celery.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-
-# Start Celery Worker with optimized memory usage
-celery -A myshop worker --loglevel=info --pool=gevent --concurrency=2 -D
-
-# Start Celery Beat in the background
+celery -A myshop worker --loglevel=info --pool=gevent -D
 celery -A myshop beat --loglevel=info --detach
-
-# Keep the container running indefinitely
 sleep infinity
