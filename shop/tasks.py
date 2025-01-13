bd0ff1654b4132bde7e5d11a194350b98a761350
sergeyitaly <sergeyitaly@yahoo.com>
2025-01-13 23:06:10 +0200

    shop product stock update

--- shop/tasks.py
+++ shop/tasks.py
@@ -3,8 +3,7 @@ from shop.models import Product
 
 @shared_task
 def increase_stock_for_unavailable_products():
-    products = Product.objects.filter(available=False)
-    for product in products:
-        product.stock = 10
-        product.save()
-    return f"Updated stock for {products.count()} unavailable products."
+    # Perform a bulk update for all unavailable products
+    updated_count = Product.objects.filter(available=False).update(stock=10, available=True)
+        # Log the number of updated products
+    return f"Updated stock for {updated_count} unavailable products."
